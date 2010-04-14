19d90b98d007bffa5ffe19f57d7e29787a36a2e9
Tobias McNulty <tobias@caktusgroup.com>
2010-04-14 10:14:52 +0200

    dummy commit

--- settings.py
+++ settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = (
     "django.contrib.sessions",
     "django.contrib.contenttypes",
     "django.contrib.auth",
+
     "rapidsms",
     "rapidsms.contrib.ajax", 
     "rapidsms.contrib.httptester", 
