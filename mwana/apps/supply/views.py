674ad5bac628a3e105c0028e5e3e8241f57a15b6
Cory Zue <czue@dimagi.com>
2010-04-15 14:33:36 +0200

    Adding single location view for supply app.

--- mwana/apps/supply/views.py
+++ mwana/apps/supply/views.py
@@ -4,11 +4,13 @@ from apps.supply.models import SupplyRequest
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.templatetags.tabs_tags import register_tab
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_GET
 from rapidsms.contrib.locations.models import Location
 from rapidsms.utils import render_to_response, web_message
+from datetime import datetime
 
 @register_tab(caption="Supplies")
+@require_GET
 def dashboard(request):
     """Supply dashboard"""
     active_requests = SupplyRequest.active().order_by("-created").order_by("location")
@@ -20,14 +22,16 @@ def dashboard(request):
                                "locations": locations })
 
 @require_http_methods(["GET", "POST"])
-def details(request, request_pk):
-    """Supply details view"""
+def request_details(request, request_pk):
+    """Supply request details view"""
     sreq = get_object_or_404(SupplyRequest, id=request_pk)
     
     if request.method == "POST":
         form = SupplyRequestForm(request.POST, instance=sreq)
         if form.is_valid():
-            supply = form.save()
+            supply = form.save(commit=False)
+            supply.modified = datetime.utcnow()
+            supply.save()
             return web_message(request,
                                "Supply request %d status set to %s" % \
                                (supply.pk, supply.get_status_display()),
@@ -38,4 +42,15 @@ def details(request, request_pk):
     
     return render_to_response(request, "supply/single_request.html", 
                                   {"sreq": sreq, "form": form})
+
+@require_GET
+def location_details(request, location_pk):
+    """Supply location details view"""
+    loc = get_object_or_404(Location.objects.select_related(depth=3), 
+                            pk=location_pk)
     
+    # this is sneaky, but allows us to access this list from
+    # template tags without doing extra querying.
+    loc.active_requests = SupplyRequest.active().filter(location=loc)
+    return render_to_response(request, "supply/single_location.html", 
+                              {"location": loc} )
