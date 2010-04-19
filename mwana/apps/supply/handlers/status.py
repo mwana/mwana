02736090fc9232f1f0e47ab689b73431c9e2f762
tsinkala <sinkalation@gmail.com>
2010-04-19 15:36:39 +0200

    Minor change in stock-out status request. Standardising response message to unregistered requesters

--- mwana/apps/supply/handlers/status.py
+++ mwana/apps/supply/handlers/status.py
@@ -6,6 +6,7 @@ from mwana.apps.supply.models import STATUS_CHOICES
 from mwana.apps.supply.models import SupplyRequest
 from mwana.apps.supply.models import SupplyType
 from rapidsms.contrib.handlers import KeywordHandler
+from mwana.apps.registration.handlers.register import RegisterHandler
 import re
 
 class StatusHandler(KeywordHandler):
@@ -29,36 +30,35 @@ class StatusHandler(KeywordHandler):
         return toReturn
 
     def handle(self, text):
+        #If contact is not registered inform them and do not proceed to check requests
+        if self.msg.contact == None:
+                self.respond("Seems you are not registered. %s" % RegisterHandler.HELP_TEXT)
+                return
+
         supply_items = set(re.findall('\S+', text))
         request = None
-        status = ""
-        name = ""
         supply_requests=None
-        if self.msg.contact == None:
-            self.respond("Seems you are not registered")
-        else:
-            try:
-                for supply_item in supply_items:
-                    try:
-                        supply_type = SupplyType.objects.get(slug=supply_item)
-                    except:
-                        self.respond("Supply %s not found." % (supply_item))
-                        continue
+        try:
+            for supply_item in supply_items:
+                try:
+                    supply_type = SupplyType.objects.get(slug=supply_item)
+                except:
+                    self.respond("Supply %s not found." % (supply_item))
+                    continue
 
-                    supply_requests = SupplyRequest.active().filter(type=supply_type)
-                    if supply_requests:
-                        match = False
-                        for request in supply_requests:
-                            if request.requested_by:
-                                if request.requested_by == self.msg.contact:
-                                    self.respond("Your request for %(supply)s has status: %(status)s as of %(date)s.",
-                                                 supply=request.type.name, status=request.get_status_display().upper(), 
-                                                 date=request.modified.strftime("%B %d, %Y at %I:%M:%S %p"))
-                                    match = True
-                        if not match:
-                            self.respond("Request for %s by %s not found" % (supply_type, self.msg.contact))
-                    else:
+                supply_requests = SupplyRequest.active().filter(type=supply_type)
+                if supply_requests:
+                    match = False
+                    for request in supply_requests:
+                        if request.requested_by:
+                            if request.requested_by == self.msg.contact:
+                                self.respond("Your request for %(supply)s has status: %(status)s as of %(date)s.",
+                                             supply=request.type.name, status=request.get_status_display().upper(),
+                                             date=request.modified.strftime("%B %d, %Y at %I:%M:%S %p"))
+                                match = True
+                    if not match:
                         self.respond("Request for %s by %s not found" % (supply_type, self.msg.contact))
-            except Exception, e:
-                print "Exception: ", e
-
+                else:
+                    self.respond("Request for %s by %s not found" % (supply_type, self.msg.contact))
+        except Exception, e:
+            print "Exception: ", e
