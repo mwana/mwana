756854f12668231d35f6c3c48ef2c5da5b638cbe
Tobias McNulty <tobias@caktusgroup.com>
2010-04-21 18:55:49 +0200

    zone_code should be null=True

--- mwana/apps/reminders/extensions/rapidsms/contact.py
+++ mwana/apps/reminders/extensions/rapidsms/contact.py
@@ -5,11 +5,12 @@
 from django.db import models
 
 class ContactZone(models.Model):
-    zone_code = models.SmallIntegerField(blank=True, help_text="An optional "
+    zone_code = models.SmallIntegerField(help_text="An optional "
                                          "code that indicates the zone"
                                          "in the catchment area of the "
                                          "contact's clinic for which he or "
-                                         "she is responsible.")
+                                         "she is responsible.", blank=True,
+                                         null=True, )
 
     class Meta:
         abstract = True
