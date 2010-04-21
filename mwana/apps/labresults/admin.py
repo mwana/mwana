c5cb2fb92f81bbcfd6e5e90ce20b5e2894c35f34
Cory Zue <czue@dimagi.com>
2010-04-21 15:57:51 +0200

    fixing admin for lab results

--- mwana/apps/labresults/admin.py
+++ mwana/apps/labresults/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from mwana.apps.labresults.models import *
 
-admin.site.register(Clinic)
-admin.site.register(Recipient)
 admin.site.register(Result)
 admin.site.register(RawResult)
