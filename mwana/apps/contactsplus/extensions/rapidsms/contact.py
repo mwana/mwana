16d7b3327e9b91e765d233dc9cedb7de76dd0b4f
Cory Zue <czue@dimagi.com>
2010-04-23 13:01:02 +0200

    adding an is_active flag to contacts plus + migration script

--- mwana/apps/contactsplus/extensions/rapidsms/contact.py
+++ mwana/apps/contactsplus/extensions/rapidsms/contact.py
@@ -8,6 +8,7 @@ class ContactType(models.Model):
     
     types = models.ManyToManyField('contactsplus.ContactType',
                                    related_name='contacts', blank=True)
+    is_active = models.BooleanField(default=True)
     
     class Meta:
         abstract = True
