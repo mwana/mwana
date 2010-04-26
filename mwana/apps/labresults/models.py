8cc76b288fc50a24e8e0cab0100c492df78b9ecb
Drew Roos <droos@dimagi.com>
2010-04-26 23:06:05 +0200

    parse full json payload from ndola lab

--- mwana/apps/labresults/models.py
+++ mwana/apps/labresults/models.py
@@ -1,42 +1,101 @@
 from django.db import models
 from rapidsms.contrib.locations.models import Location
 from rapidsms.models import Connection
-
-class RawResult(models.Model):
-    date = models.DateTimeField()
-    processed = models.BooleanField('Whether or not this result was saved to '
-                                    'the final results table in the database',
-                                    default=False)
-    data = models.TextField()
-    parsed = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return '%s (%s)' % (self.date, self.processed and 'saved' or 'unsaved')
-
+from django.contrib.auth.models import User
 
 class Result(models.Model):
     RESULT_CHOICES = (
         ('P', 'HIV Positive'),
         ('N', 'HIV Negative'),
-        ('B', 'Bad Sample'),
+        ('R', 'Rejected Sample'),   #this could include samples that failed to give a definitive result
+                                    #  i.e., 'rejected -- indeterminate'
+        ('I', 'Indeterminate'),     #could mean 'unable to get definitive result', but most likely means
+                                    #  'sample on hold while we investigate missing critical data, such
+                                    #  as originating facility'; if data cannot be found, sample will be
+                                    #  ultimately rejected. we should sit on these for a while before we
+                                    #  send them out
+        ('X', 'Inconsistent'),      #means the source database has record in a state that doesn't make
+                                    #  sense; sit on it indefinitely, hoping it will resolve to a
+                                    #  different status
     )
 
     STATUS_CHOICES = (
-        ('in-transit', 'En route to lab'),
+        ('in-transit', 'En route to lab'),      #not supported currently
         ('unprocessed', 'At lab, not processed yet'),
         ('new', 'Fresh result from lab'),
         ('notified', 'Clinic notified of new result'),
         ('sent', 'Result sent to clinic'),
+        ('updated', 'Updated data from lab')    #set when server receives updates to a sample record
+                                                #AFTER this result has already been sent to the clinic.
+                                                #if result has not yet been sent, it keeps status 'new'.
+                                                #the updated data may or may not merit sending the
+                                                #update to the clinic (i.e., changed result, yes, changed
+                                                #child age, no)
     )
 
-    patient_id = models.CharField(max_length=30, blank=True)
-    sample_id = models.CharField(max_length=30)
+    SEX_CHOICES = (
+        ('m', 'Male'),
+        ('f', 'Female'),
+    )
+
+    sample_id = models.CharField(primary_key=True, max_length=10)    #lab-assigned sample id
+    requisition_id = models.CharField(max_length=50)   #non-standardized format varying by clinic; could be patient
+                                                       #id, clinic-assigned sample id, or even patient name
     clinic = models.ForeignKey(Location)
-    result = models.CharField(choices=RESULT_CHOICES, max_length=1)
-    taken_on = models.DateField(null=True)
-    entered_on = models.DateField()
+
+    result = models.CharField(choices=RESULT_CHOICES, max_length=1, null=True)  #null == 'not tested yet'
+    result_detail = models.CharField(max_length=200, null=True)   #reason for rejection or explanation of inconsistency
+    
+    collected_on = models.DateField(null=True)   #date collected at clinic
+    entered_on = models.DateField(null=True)     #date received at lab
+    processed_on = models.DateField(null=True)   #date tested at lab
+    #all date fields are entered by lab -- not based on date result entered or such
+    
     notification_status = models.CharField(choices=STATUS_CHOICES, max_length=15)
 
-    def __unicode__(self):
-        return '%s/%s/%s %s (%s)' % (self.patient_id, self.sample_id, self.clinic.slug, self.result, self.notification_status)
+    #ancillary demographic data that can help matching up results back to patients
+    birthdate = models.DateField(null=True)
+    child_age = models.IntegerField(null=True)  #age in weeks
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, null=True)
+    mother_age = models.IntegerField(null=True) #age in years
+    collecting_health_worker = models.CharField(max_length=100, null=True)
+    coll_hw_title = models.CharField(max_length=30, null=True)
 
+    def __unicode__(self):
+        return '%s/%s/%s %s (%s)' % (self.requisition_id, self.sample_id, self.clinic.slug,
+                                     self.result if self.result != None else '-', self.notification_status)
+
+
+class Payload(models.Model):
+    incoming_date = models.DateTimeField()
+    auth_user = models.ForeignKey(User)
+    
+    version = models.CharField(max_length=10, null=True)
+    source = models.CharField(max_length=50, null=True)
+    client_timestamp = models.DateTimeField(null=True)
+    info = models.CharField(max_length=50, null=True)
+    
+    parsed_json = models.BooleanField(default=False)
+    validated_schema = models.BooleanField(default=False)
+    
+    raw = models.TextField()
+    
+    def __unicode__(self):
+        return 'from %s/%s at %s (%sb)' % (self.source if self.source != None else '-',
+                                           self.version if self.version != None else '-',
+                                           self.incoming_date.strftime('%Y-%m-%d %H:%M:%S'),
+                                           len(self.raw))
+
+class LabLog(models.Model):
+    timestamp = models.DateTimeField(null=True)
+    message = models.TextField(null=True)
+    level = models.CharField(max_length=20, null=True)
+    line = models.IntegerField(null=True)
+    
+    payload_id = models.ForeignKey(Payload)
+    raw = models.TextField(null=True)
+    
+    def __unicode__(self):
+        return ('%d: %s> %s' % (self.line, self.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
+                                self.message if len(self.message) < 20 else (self.message[:17] + '...'))) \
+            if self.raw != None else 'parse error'
--- mwana/apps/labresults/views.py
+++ mwana/apps/labresults/views.py
@@ -1,50 +1,240 @@
 import json
-import datetime
+from datetime import datetime, date, timedelta
+import logging
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.forms import ModelForm
 
 from mwana.apps.labresults import models as labresults
 from mwana.decorators import has_perm_or_basicauth
 
+from rapidsms.contrib.locations.models import Location
+
+def json_datetime (val):
+    try:
+        return datetime.strptime(val, '%Y-%m-%d %H:%M:%S')
+    except:
+        return None
+    
+def json_date (val):
+    try:
+        return date.strptime(val, '%Y-%m-%d')
+    except:
+        return None
+
+def json_timestamp (val):
+    if val[-4] in ('.', ','):
+        return None
+    
+    try:
+        dt = datetime.strptime(val[:-4], '%Y-%m-%d %H:%M:%S')
+        return dt + timedelta(microseconds=1000*int(val[-3:]))
+    except:
+        return None
+
+def dictval (dict, val, trans=lambda x: x):
+    return trans(dict[val]) if val in dict else None
 
 @require_http_methods(['POST'])
 @has_perm_or_basicauth('labresults.add_rawresult', 'Lab Results')
 def accept_results(request):
-    """
-    Accepts incoming results from the lab via HTTP POST.  Example usage:
-    
-    import urllib
-    import urllib2
-    
-    # set up authentication info
-    authinfo = urllib2.HTTPBasicAuthHandler()
-    authinfo.add_password(realm='Lab Results',
-                          uri='http://localhost:8000/',
-                          user='adh',
-                          passwd='abc')
-    
-    opener = urllib2.build_opener(authinfo)
-    urllib2.install_opener(opener)
-    data = urllib.urlencode({'varname': 'value'})
-    f = urllib2.urlopen('http://localhost:8000/labresults/incoming/', data)
-    """
+    """Accepts incoming results from the lab via HTTP POST.  see
+    connection() in extract.py for how to submit"""
+    logger = logging.getLogger('mwana.apps.labresults.views.accept_results')
     
     if request.META['CONTENT_TYPE'] != 'text/json':
-        #do something?
-        pass
+        logger.warn('incoming post does not have text/json content type')
     
     content = request.raw_post_data
     
+    payload_date = datetime.now()
+    payload_user = request.user
     try:
-      json.loads(request.raw_post_data)
-      parsed = True
+        data = json.loads(content)
     except:
-      parsed = False
+        #if payload does not parse as valid json, save raw content and return error
+        payload = labresults.Payload(incoming_date=payload_date,
+                                     auth_user=payload_user,
+                                     parsed_json=False,
+                                     raw=content)
+        payload.save()
+        return HttpResponse('CANNOT PARSE (%d bytes received)' % len(content))
     
-    labresults.RawResult.objects.create(date=datetime.datetime.now(), data=content, parsed=parsed)
+    #safety -- no matter what else happens, we'll have the original data
+    payload = labresults.Payload(incoming_date=payload_date,
+                                 auth_user=payload_user,
+                                 parsed_json=True,
+                                 raw=content)
+    try:
+        payload.save()
+    except:
+        #failed to save raw data; attempt to leave it in the log
+        logger.exception('could not save raw incoming data from DBS lab!! raw data: %s' % content)
+        raise
     
-    if parsed:
-      return HttpResponse('SUCCESS')
+    #parse/save the individual result and logs entries; aggregate whether all succeeded, or if any
+    #record failed to validate
+    records_validate = all(accept_record(r) for r in data['records']) if 'records' in data else False
+    logs_validate = all(accept_log(l, payload.id) for l in data['logs']) if 'logs' in data else False
+    
+    meta_fields = {
+        'version': dictval(data, 'version'),
+        'source': dictval(data, 'source'), 
+        'client_timestamp': dictval(data, 'now', json_datetime),
+        'info':  dictval(data, 'info'),
+    }
+    
+    f_payload = PayloadForm(meta_fields, instance=payload)
+    if f_payload.is_valid():
+        payload = f_payload.save(commit=False)
+        payload.validated_schema = (records_validate and logs_validate)
+        payload.save()
     else:
-      return HttpResponse('CANNOT PARSE (%d bytes received)' % len(content))
+        logger.error('errors in json schema for payload: ' + str(f_payload.errors))
+                
+    return HttpResponse('SUCCESS')
+     
+def normalize_clinic_id (zpct_id):
+    return zpct_id[:-1] if zpct_id[-1] == '0' and len(zpct_id) > 3 else zpct_id
+     
+def map_result (verbose_result):
+    result_codes = {'positive': 'P',
+                    'negative': 'N',
+                    'rejected': 'R',
+                    'indeterminate': 'I',
+                    'inconsistent': 'X'}
+    return result_codes[verbose_result] if verbose_result in result_codes else ('x-' + verbose_result)
+     
+def accept_record (record):
+    logger = logging.getLogger('mwana.apps.labresults.views.accept_record')
+    
+    #retrieve existing record for id, if it exists
+    sample_id = dictval(record, 'id')
+    old_record = None
+    if sample_id != None:
+        try:
+            old_record = labresults.Result.objects.get(sample_id=sample_id)
+        except labresults.Result.DoesNotExist:
+            pass
+    
+    def cant_save (message):
+        message = 'cannot save record: ' + message
+        if old_record != None:
+            message += '; original record [%s] untouched' % sample_id
+        message += '\nrecord: %s' % str(record)
+        logger.error(message)
+    
+    #validate required identifying fields
+    for reqd_field in ('id', 'pat_id', 'fac'):
+        if dictval(record, reqd_field) == None:
+            cant_save('required field %s missing' % reqd_field)
+            return False
+    
+    #validate clinic id
+    clinic_id = normalize_clinic_id(dictval(record, 'fac'))
+    try:
+        clinic_obj = Location.objects.get(slug=clinic_id)
+    except Location.DoesNotExist:
+        cant_save('clinic id %s is not a recognized clinic' % clinic_id)
+        return False
+        
+    #general field validation
+    record_fields = {
+        'sample_id': sample_id,
+        'requisition_id': dictval(record, 'pat_id'),
+        'clinic': clinic_obj,
+        'result': dictval(record, 'result', map_result),
+        'result_detail': dictval(record, 'result_detail'),
+        'collected_on': dictval(record, 'coll_on', json_date),
+        'entered_on': dictval(record, 'recv_on', json_date),
+        'processed_on': dictval(record, 'proc_on', json_date),
+        'birthdate': dictval(record, 'dob', json_date),
+        'child_age': dictval(record, 'child_age'),
+        'sex': dictval(record, 'sex'),
+        'mother_age': dictval(record, 'mother_age'),
+        'collecting_health_worker': dictval(record, 'hw'),
+        'coll_hw_title': dictval(record, 'hw_tit'),
+    }
+    f_result = ResultForm(record_fields)
+    if f_result.is_valid():
+        new_record = f_result.save(commit=False)
+    else:
+        cant_save('validation errors in record: %s' % str(f_result.errors))
+        return False
+
+    #validate record sync status (couldn't validate with the rest of the fields because has no
+    #direct analogue in the model)
+    rec_status = dictval(record, 'sync')
+    if rec_status not in ('new', 'update'):
+        cant_save('sync_status not an allowed value')
+        return False
+
+    if old_record == None:
+        if rec_status == 'update':
+            logger.info('received a record update for a result that doesn\'t exist in the model; original record may not have validated; treating as new record...')
+        
+        new_record.notification_status = 'new' if new_record.result != None else 'unprocessed'
+    else:
+        new_record.notification_status = old_record.notification_status
+
+        #change to requisition id
+        if old_record.notification_status == 'sent' and old_record.requisition_id != new_record.requisition_id:
+            logger.warning('requisition id in record [%s] has changed (%s -> %s)! how do we handle this?' %
+                           (sample_id, old_record.requisition_id, new_record.requisition_id))
+
+        #change to clinic
+        if old_record.notification_status in ('sent', 'notified') and old_record.clinic != new_record.clinic:
+            logger.warning('clinic id in record [%s] has changed (%s -> %s)! how do we handle this?' %
+                           (sample_id, old_record.clinic.slug, new_record.clinic.slug))
+
+        #change to test result
+        if old_record.result == None and new_record.result != None:
+            new_record.notification_status = 'new'   #sample was processed by lab
+        elif old_record.notification_status == 'sent' and old_record.result != new_record.result:
+            logger.info('already-sent result for record [%s] has changed! need to notify of update' % sample_id)
+            new_record.notification_status = 'updated'
+                
+    new_record.save()
+    return True
+    
+
+
+def accept_log (log, payload_id):
+    logger = logging.getLogger('mwana.apps.labresults.views.accept_log')
+    
+    logentry = labresults.LabLog(payload_id=payload_id)
+
+    logfields = {
+        'timestamp': dictval(log, 'at', json_timestamp),
+        'message': dictval(log, 'msg'),
+        'level': dictval(log, 'lvl'),
+        'line': dictval(log, 'ln')
+    }
+
+    f_log = LogForm(logfields, instance=logentry)
+    if f_log.is_valid():
+        f_log.save()
+        return True
+    else:
+        logger.error('errors in json schema for log: ' + str(f_log.errors))
+        logentry.raw = str(log)
+        logentry.save()
+        return False
+     
+
+class PayloadForm(ModelForm):
+    class Meta:
+        model = labresults.Payload
+        fields = ['version', 'source', 'client_timestamp', 'info']
+
+class LogForm(ModelForm):
+    class Meta:
+        model = labresults.LabLog
+        fields = ['timestamp', 'message', 'level', 'line']
+        
+class ResultForm(ModelForm):
+    class Meta:
+        model = labresults.Result
+        exclude = ['notification_status']
+        
