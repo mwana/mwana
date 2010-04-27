c1e0566ef2dc8edbce15af572e9d36ec7b531e9d
Tobias McNulty <tobias@caktusgroup.com>
2010-04-27 12:17:01 +0200

    removing location_unique migration

--- mwana/apps/location_importer/migrations/002_location_unique.sql
+++
@@ -1,5 +0,0 @@
--- make location slug unique only across the location type
-BEGIN;
-ALTER TABLE locations_location DROP CONSTRAINT locations_location_slug_key;
-ALTER TABLE locations_location ADD UNIQUE ("slug", "type_id");
-COMMIT;
