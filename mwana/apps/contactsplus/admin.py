24589ef1dc63d21c0f6a93a1b623f22aaddbfedb
Tobias McNulty <tobias@caktusgroup.com>
2010-04-28 11:00:58 +0200

    added default_connection to contact list_display

--- mwana/apps/contactsplus/admin.py
+++ mwana/apps/contactsplus/admin.py
@@ -7,8 +7,8 @@ from mwana.apps.contactsplus import models as contactsplus
 
 admin.site.unregister(Contact)
 class ContactAdmin(ContactAdmin):
-    list_display = ('unicode', 'alias', 'language', 'location', 'types_list',
-                    'is_active',)
+    list_display = ('unicode', 'alias', 'language', 'location',
+                    'default_connection', 'types_list', 'is_active',)
     list_filter = ('types', 'is_active', 'language',)
     list_editable = ('is_active',)
     search_fields = ('name', 'alias',)
