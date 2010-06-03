dac97fb3f72e473f18b7b8476adebc2e3bb6931a
tsinkala <sinkalation@gmail.com>
2010-06-03 16:55:26 +0200

    adding semi-collon last alter statement in migration script so that script is syntactically valid on postgresql as well

--- mwana/apps/labresults/migrations/005_result.sql
+++ mwana/apps/labresults/migrations/005_result.sql
@@ -2,5 +2,5 @@ BEGIN;
 ALTER TABLE labresults_result
 ADD COLUMN record_change varchar(6);
 ALTER TABLE labresults_result
-ADD COLUMN old_value varchar(50)
+ADD COLUMN old_value varchar(50);
 COMMIT;
