c3d89dab6d33d2ab24fa4ba7037201f4f367bf46
tsinkala <sinkalation@gmail.com>
2010-06-03 15:30:39 +0200

    improving on logging messages: changing logger.debug('in send_changed_records_notification') to logger.info('checking changed records'); changing print statement to logger.info(); removing extra logging statement

--- mwana/apps/labresults/tasks.py
+++ mwana/apps/labresults/tasks.py
@@ -27,14 +27,13 @@ def send_results_notification(router):
         
 def send_changed_records_notification(router):
     if settings.SEND_LIVE_LABRESULTS:
-        logger.debug('in send_changed_records_notification')
+        logger.info('checking changed records')
         clinics_with_results =\
           Location.objects.filter(lab_results__notification_status__in=
                                   ['updated', 'notified']).distinct()
         labresults_app = router.get_app(const.LAB_RESULTS_APP)
         for clinic in clinics_with_results:
             logger.info('notifying %s of changed results' % clinic)
-            logger.debug('notifying %s of changed results' % clinic)
             labresults_app.notify_clinic_of_changed_records(clinic)
     else:
         logger.info('not notifying any clinics of changed results because '
