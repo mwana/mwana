230b9db0d4c629822357ee2cd25ba1e13fceab1d
tsinkala <sinkalation@gmail.com>
2010-09-09 18:53:13 +0200

    adding a report admin page to display a reporting view for turnarrounds/. This will allow filtering by distict, facility, month, year, day and also sorting by any field

--- mwana/apps/reports/migrations/001_postgresql_turnarround.sql
+++ mwana/apps/reports/migrations/001_postgresql_turnarround.sql
@@ -1,9 +1,14 @@
+DROP TABLE reports_turnarround;
+
+CREATE VIEW reports_turnarround
+AS
 SELECT
-     district.name as District,
-     locations_location.name as Facility,
+     labresults_result.id,
+     district.name as district,
+     locations_location.name as facility,
      (entered_on-collected_on)+1 transporting,
-     (processed_on-entered_on)+1 testing,
-     (date(labresults_payload.incoming_date)-(processed_on)) +1 staging,
+     (processed_on-entered_on)+1 processing,
+     (date(labresults_payload.incoming_date)-(processed_on)) +1 delays,
      (date(result_sent_date)-date(labresults_payload.incoming_date)) +1 retrieving,
      (date(result_sent_date)-collected_on)+1 turnarround,
      date(result_sent_date) date
@@ -13,6 +18,6 @@ FROM
      join locations_location on locations_location.id=labresults_result.clinic_id
      join locations_location as district on locations_location.parent_id=district.id
 WHERE
-     notification_status = 'sent'
+     result IS NOT null
 ORDER BY
      result_sent_date ASC
--- mwana/apps/reports/models.py
+++ mwana/apps/reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Turnarround(models.Model):
+    """
+    A stub to display a view in django admin format for turnarroud data
+    """
+    district = models.CharField(max_length=50)
+    facility = models.CharField(max_length=100)
+    transporting = models.IntegerField(blank=True, null=True)
+    processing = models.IntegerField(blank=True, null=True)
+    delays = models.IntegerField(blank=True, null=True)
+    retrieving = models.IntegerField(blank=True, null=True)
+    turnarround = models.IntegerField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
