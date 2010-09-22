7e0b584c767547a333027ad16cc489803f24300a
tsinkala <sinkalation@gmail.com>
2010-09-22 14:00:02 +0200

    temporally disabling tabs except dashboard, reports, maps

--- mwana/settings_moh_zambia.py
+++ mwana/settings_moh_zambia.py
@@ -2,11 +2,11 @@ from mwana.settings import *
 
 # temporarily disable the entire project in the production environment
 # until we get approval
-INSTALLED_APPS.insert(INSTALLED_APPS.index("rapidsms"),
-                      "mwana.apps.whitelist")
-WHITELIST_RESPONSE = "I'm sorry, Results160 and RemindMi are still in testing "\
-                     "phase. We will notify you when the system is live."
-SEND_LIVE_LABRESULTS = False
+#INSTALLED_APPS.insert(INSTALLED_APPS.index("rapidsms"),
+#                      "mwana.apps.whitelist")
+#WHITELIST_RESPONSE = "I'm sorry, Results160 and RemindMi are still in testing "\
+#                     "phase. We will notify you when the system is live."
+SEND_LIVE_LABRESULTS = True
 
 DEBUG = False
 
@@ -29,7 +29,7 @@ LANGUAGE_CODE = 'bem-zm'
 INSTALLED_BACKENDS.update({
     "pygsm" : {"ENGINE": "rapidsms.backends.gsm",
                "port": "/dev/ttyUSB0",
-               'baudrate': '115200', 
+               'baudrate': '115200',
                'rtscts': '1',
                'timeout': 10}
 })
@@ -45,6 +45,20 @@ DJANGO_LOG_FILE = "/var/log/rapidsms/rapidsms.django.log"
 LOG_FORMAT = "[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s"
 LOG_SIZE = 1000000 # in bytes
 LOG_BACKUPS = 256     # number of logs to keep around
+
+# TODO add commented tabs to respective urls.py files
+TABS = [
+    ('rapidsms.views.dashboard', 'Dashboard'),
+#    ('rapidsms.contrib.httptester.views.generate_identity', 'Message Tester'),
+    ('rapidsms.contrib.locations.views.dashboard', 'Map'),
+#    ('rapidsms.contrib.messagelog.views.message_log', 'Message Log'),
+#    ('rapidsms.contrib.messaging.views.messaging', 'Messaging'),
+#    ('rapidsms.contrib.registration.views.registration', 'Registration'),
+#    ('rapidsms.contrib.scheduler.views.index', 'Event Scheduler'),
+#    ('mwana.apps.supply.views.dashboard', 'Supplies'),
+#    ('mwana.apps.labresults.views.dashboard', 'Results160'),
+    ('mwana.apps.labresults.views.mwana_reports', 'Reports'),
+]
 
 from mwana.logconfig import init_email_handler
 init_email_handler(EMAIL_HOST, DEFAULT_FROM_EMAIL, ADMINS,
