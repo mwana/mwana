7e987f44853565db4ee862b66c04f8054ca61dad
tsinkala <sinkalation@gmail.com>
2010-10-12 14:30:24 +0200

    adding an admin page to group messages by:  'date', 'text', 'direction', 'contact_type','keyword', 'backend', 'changed_res',  'new_results', 'app', 'clinic', 'phone' to migrate use python manage.py dbshell < apps\reports\migrations\002_message_group.sql

--- mwana/apps/reports/admin.py
+++ mwana/apps/reports/admin.py
@@ -5,11 +5,22 @@ from mwana.apps.reports.models import SupportedLocation
 
 class TurnaroundAdmin(admin.ModelAdmin):
     list_display = ('district', 'facility', 'transporting', 'processing',
-                    'delays', 'date_reached_moh','retrieving', 'date_retrieved',
+                    'delays', 'date_reached_moh', 'retrieving', 'date_retrieved',
                     'turnaround')
     date_hierarchy = 'date_retrieved'
     list_filter = ('date_retrieved', 'district', 'facility')
 admin.site.register(Turnaround, TurnaroundAdmin)
+
+class MessageGroupAdmin(admin.ModelAdmin):
+    list_display = ('date', 'text', 'direction', 'contact_type',
+                    'keyword', 'backend', 'changed_res',
+                    'new_results', 'app', 'clinic', 'phone')
+    date_hierarchy = 'date'
+    list_filter = ('direction', 'contact_type', 'keyword', 'backend',
+                   'new_results', 'changed_res','clinic', 'phone')
+    search_fields = ('text', )
+admin.site.register(MessageGroup, MessageGroupAdmin)
+#admin.site.register(MessageGroup)
 
 class SupportedLocationAdmin(admin.ModelAdmin):
     list_display = ('location', 'supported')
