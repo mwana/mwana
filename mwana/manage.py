e9424cfdce95db4f0075eb40937ba03ca4c87610
Anton de Winter <adewinter@dimagi.com>
2010-10-26 08:43:48 -0400

    reverted manage.py script to include nifty settings file loading script.  Removed pycharm .idea folder.

--- mwana/manage.py
+++ mwana/manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-#import sys, os
-
+import sys, os
 from django.core.management import execute_manager
 import settings
+
+# use a default settings module if none was specified on the command line
+DEFAULT_SETTINGS = 'mwana.localsettings'
+settings_specified = any([arg.startswith('--settings=') for arg in sys.argv])
+if not settings_specified and len(sys.argv) >= 2:
+    print "NOTICE: using default settings module '%s'" % DEFAULT_SETTINGS
+    sys.argv.append('--settings=%s' % DEFAULT_SETTINGS)
 
 
 if __name__ == "__main__":
