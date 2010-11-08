60ba6a6244939f1f475e73fdb90c5f510af9a87e
Tobias McNulty <tobias@caktusgroup.com>
2010-11-08 12:03:09 +0200

    remove commented settings

--- mwana/malawi/settings_malawi.py
+++ mwana/malawi/settings_malawi.py
@@ -1,18 +1,5 @@
 from mwana.settings_project import *
 
-# Zambia:
-#RESULTS160_SLUGS = {
-#    'CBA_SLUG' = 'cba',
-#    'PATIENT_SLUG': 'patient',
-#    'CLINIC_WORKER_SLUG': 'worker',
-#    # location types:
-#    'CLINIC_SLUGS': ('urban_health_centre', '1st_level_hospital',
-#                    'rural_health_centre', 'health_post'),
-#    'ZONE_SLUGS': ('zone',),
-#    'DISTRICT_SLUGS': ('districts',), # XXX verify me
-#    'PROVINCE_SLUGS': ('provinces',), # XXX verify me
-#}
-
 # Malawi:
 RESULTS160_SLUGS = {
     'CBA_SLUG': 'cba',
