7ca706b8a25daef66f441c28e146118e902006a1
Trevor Sinkala <sinkalation@gmail.com>
2010-11-09 15:44:23 +0200

    updating model and admin for patient tracing. linking patient trace with patient event

--- mwana/apps/patienttracing/admin.py
+++ mwana/apps/patienttracing/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from mwana.apps.patienttracing.models import PatientTrace
 
 class PatientTraceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'patient', 'cba', 'status', 'start_date', 'sent_date'
-                    , 'confirmed_date',)
-    list_filter = ('cba', 'start_date', 'sent_date', 'confirmed_date')
-    search_fields = ('name', 'cba',)
+    list_display = ('name', 'patient_event', 'messenger', 'confirmed_by', 'status', 'start_date',
+                     'confirmed_date',)
+    list_filter = ('messenger','confirmed_by', 'start_date', 'reminded_on', 'confirmed_date')
+    search_fields = ('name', 'messenger','confirmed_by')
     date_hierarchy = 'confirmed_date'
 admin.site.register(PatientTrace, PatientTraceAdmin)
 
--- mwana/apps/patienttracing/models.py
+++ mwana/apps/patienttracing/models.py
@@ -2,32 +2,40 @@ import datetime
 
 from django.db import models
 from rapidsms.models import Contact
+from mwana.apps.reminders.models import PatientEvent
 
 STATUS_CHOICES = (
-                  ("N", "new"),
-                  ("T", "told"),
-                  ("C", "confirmed"),
-                  ("L", "lost to followup"),# when mother can't be traced
+                  ("new", "new"),
+                  ("told", "told"),
+                  ("confirmed", "confirmed"),
+                  ("refused", "patient refused"),# when mother can't be traced
+                  ("lost", "lost to followup"),# when mother can't be traced
+                  ("dead", "patient died"),# when mother can't be traced
                   )
+
 
 class PatientTrace(models.Model):
     # person who initiates the tracing
     initiator = models.ForeignKey(Contact, related_name='patients_traced',
-                                     limit_choices_to={'types__slug': 'clinic_worker'})
+                                     limit_choices_to={'types__slug': 'clinic_worker'},
+                                     null=True, blank=True)
+    type = models.CharField(max_length=15)
     name = models.CharField(max_length=50) # name of patient to trace
-    patient = models.ForeignKey(Contact, related_name='patient_traces',
+    patient_event = models.ForeignKey(PatientEvent, related_name='patient_traces',
                                 limit_choices_to={'types__slug': 'patient'},
-                                null=True, blank=True) # for probable future use
+                                null=True, blank=True) 
     messenger = models.ForeignKey(Contact,  related_name='patients_reminded',
-                            limit_choices_to={'types__slug': 'cba'})# cba who informs patient
+                            limit_choices_to={'types__slug': 'cba'}, null=True,
+                            blank=True)# cba who informs patient
 
     confirmed_by = models.ForeignKey(Contact, related_name='patient_traces_confirmed',
-                            limit_choices_to={'types__slug': 'cba'})# cba who confirms that patient visited clinic
+                            limit_choices_to={'types__slug': 'cba'}, null=True,
+                            blank=True)# cba who confirms that patient visited clinic
 
-    status = models.CharField(choices=STATUS_CHOICES, max_length=1) # status of tracing activity  
+    status = models.CharField(choices=STATUS_CHOICES, max_length=15) # status of tracing activity
 
     start_date = models.DateTimeField() # date when tracing starts
-    reminded_on = models.DateTimeField() # date when cba tells mother
+    reminded_on = models.DateTimeField(null=True, blank=True) # date when cba tells mother
     confirmed_date = models.DateTimeField(null=True, blank=True)# date of confirmation that patient visited clinic   
     
 
