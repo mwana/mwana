e7d4fac44ad637e478536c9fe5b310b4bd6341cf
Tobias McNulty <tobias@caktusgroup.com>
2010-11-10 13:03:29 +0200

    remove unused scripts, submodules; add example test_localsettings file

--- mwana/get_rpts.bat
+++
@@ -1,3 +0,0 @@
-python manage.py dbshell <apps\reports\utils\rawsql\total_results.sql >result.txt
-python manage.py dbshell <apps\reports\utils\rawsql\average_turnaround.sql >>result.txt
-python manage.py dbshell <apps\reports\utils\rawsql\results_sent_by_facility.sql >>result.txt
