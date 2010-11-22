e2c8d6b3a1434a43c912c887be9603dade3916d9
Lengani Kaunda <lengani@p4studio.com>
2010-11-22 11:59:45 +0200

    fix migration for send_live_results

--- mwana/apps/labresults/migrations/009_postgresql_locations_location_send_live_results.sql
+++ mwana/apps/labresults/migrations/009_postgresql_locations_location_send_live_results.sql
@@ -1,6 +1,5 @@
 BEGIN;
 ALTER TABLE locations_location ADD COLUMN send_live_results boolean;
 UPDATE locations_location SET send_live_results = false;
-ALTER TABLE locations_location ALTER COLUMN send_live_results SET NOT NULL;
 COMMIT;
-
+ALTER TABLE locations_location ALTER COLUMN send_live_results SET NOT NULL;
