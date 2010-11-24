a31fb00eb916bddedddd1f7907504ecad052b031
tsinkala <sinkalation@gmail.com>
2010-11-24 14:23:15 +0200

    enforcing login requrement for frontend (reports,alerts, etc)

--- mwana/settings_project.py
+++ mwana/settings_project.py
@@ -164,6 +164,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'mwana.middleware.LoginRequired',
 ]
 
 
--- mwana/urls.py
+++ mwana/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     
     # RapidSMS core URLs
-    (r'^account/', include('rapidsms.urls.login_logout')),
+    (r'^accounts/', include('rapidsms.urls.login_logout')),
     url(r'^$', 'rapidsms.views.dashboard', name='rapidsms-dashboard'),
 
     # Mwana app URLs
