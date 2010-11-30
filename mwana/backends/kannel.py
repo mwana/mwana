b69a185b46f199f046902efad92f4b7f1048e272
Tobias McNulty <tobias@caktusgroup.com>
2010-11-30 16:40:04 +0200

    make kannel charset/coding configurable from settings

--- mwana/backends/kannel.py
+++ mwana/backends/kannel.py
@@ -25,13 +25,15 @@ class KannelBackend(RapidHttpBacked):
     max-messages = 0
     get-url = http://127.0.0.1:8888/?id=%p&text=%a
     """
-    charset = 'UTF-8'
-    coding = 1
 
     def configure(self, sendsms_url='http://127.0.0.1:13013/cgi-bin/sendsms',
-                  sendsms_params=None, **kwargs):
+                  sendsms_params=None, charset=None, coding=None,
+                  encode_errors=None, **kwargs):
         self.sendsms_url = sendsms_url
         self.sendsms_params = sendsms_params or {}
+        self.charset = charset or 'ascii'
+        self.coding = coding or 0
+        self.encode_errors = encode_errors or 'ignore'
         super(KannelBackend, self).configure(**kwargs)
 
     def handle_request(self, request):
@@ -64,7 +66,8 @@ class KannelBackend(RapidHttpBacked):
         self.info('Sending message: %s' % message)
         url_args = copy.copy(self.sendsms_params)
         url_args['to'] = message.connection.identity
-        url_args['text'] = message.text.encode(self.charset)
+        url_args['text'] = message.text.encode(self.charset,
+                                               self.encode_errors)
         url_args['coding'] = self.coding
         url_args['charset'] = self.charset
         url = '?'.join([self.sendsms_url, urllib.urlencode(url_args)])
--- mwana/malawi/settings_staging.py
+++ mwana/malawi/settings_staging.py
@@ -44,6 +44,8 @@ INSTALLED_BACKENDS.update({
         "sendsms_url": "http://127.0.0.1:13013/cgi-bin/sendsms",
         "sendsms_params": {"smsc": "zain-modem", "username": "rapidsms",
                             "password": ""}, # set in localsettings.py
+        "coding": 0,
+        "charset": "ascii",
     },
     "tnm" : {
         "ENGINE":  "mwana.backends.kannel",
@@ -52,6 +54,8 @@ INSTALLED_BACKENDS.update({
         "sendsms_url": "http://127.0.0.1:13013/cgi-bin/sendsms",
         "sendsms_params": {"smsc": "tnm-smpp", "username": "rapidsms",
                             "password": ""}, # set in localsettings.py
+        "coding": 0,
+        "charset": "ascii",
     }
 })
 
