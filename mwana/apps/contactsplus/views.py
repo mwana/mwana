5f60f6a55ef039a2eeca51c58a0eaf560fe516c5
Lengani Kaunda <lengani@p4studio.com>
2010-12-01 12:29:58 +0200

    remove csv export action

--- mwana/apps/contactsplus/views.py
+++ mwana/apps/contactsplus/views.py
@@ -13,7 +13,8 @@ def export_contacts(request):
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment;filename=contacts_export.csv'
     writer = csv.writer(response)
-    headings = ['Name', 'Phone','District','Facility', 'Facility Type', 'Registered', 'Contact Type']
+    headings = ['Name', 'Phone', 'District', 'Facility', 'Facility Type',
+                'Registered', 'Contact Type']
     writer.writerow(headings)
     for obj in contacts:
         row = []
@@ -26,26 +27,11 @@ def export_contacts(request):
         row.append(obj.location.parent)
         row.append(obj.location)
         row.append(obj.location.type)
-        earliest = Message.objects.filter(contact=obj.id, direction='I').aggregate(registered=Min('date'))
+        earliest = Message.objects.filter(
+            contact=obj.id,
+            direction='I',
+        ).aggregate(registered=Min('date'))
         row.append(earliest['registered'])
         row.append(";".join(obj.types.values_list('name', flat=True)))
         writer.writerow(row)
-    return response
-    
-def export_to_csv(modeladmin, request, queryset):
-    """
-    Generic csv export admin action.
-    """
-    if not request.user.is_staff:
-        raise PermissionDenied
-    opts = modeladmin.model._meta
-    response = HttpResponse(mimetype='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=%s.csv' % unicode(opts).replace('.', '_')
-    writer = csv.writer(response)
-    field_names = [field.name for field in opts.fields]
-    # Write a first row with header information
-    writer.writerow(field_names)
-    # Write data rows
-    for obj in queryset:
-        writer.writerow([getattr(obj, field) for field in field_names])
     return response
