d70ee244411d6a2daf973a5af964b38aa668498e
tsinkala <sinkalation@gmail.com>
2011-02-15 11:38:14 +0200

    minor code change

--- mwana/apps/labresults/tests.py
+++ mwana/apps/labresults/tests.py
@@ -576,14 +576,14 @@ class TestApp(LabresultsSetUp):
             clinic_worker < Sorry, I don't know about a location with code 403029. Please check your code and try again.
             clinic_worker > Reports 402029
             clinic_worker > Reports 403012
-            clinic_worker > Reports 403012 Jan
-            clinic_worker > Reports 403012 1
+            clinic_worker > Reports 403012 Feb
+            clinic_worker > Reports 403012 2
             clinic_worker > Reports 402000
             clinic_worker > Reports 403000
             clinic_worker > Reports 4030
             clinic_worker > Reports mansa
             clinic_worker > Reports 400000
-            clinic_worker > Reports 40 Jan
+            clinic_worker > Reports 40 Feb
             clinic_worker > Reports Luapula
         """.format(**self._result_text())
         self.runScript(script)        
