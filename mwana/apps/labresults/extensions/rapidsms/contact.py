326e5d7b97984363fc8311f82d2628c57e6e729e
Tobias McNulty <tobias@caktusgroup.com>
2011-03-24 12:16:33 -0400

    make alias column null=True (since it is now unique, the previous default of  would consistently violate the unique constraint)

--- mwana/apps/labresults/extensions/rapidsms/contact.py
+++ mwana/apps/labresults/extensions/rapidsms/contact.py
@@ -5,7 +5,8 @@ from django.db import models
 class ContactPin(models.Model):
     pin = models.CharField(max_length=4, blank=True,
                            help_text="A 4-digit pin code for sms authentication workflows.")
-    alias = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100, blank=True, null=True,
+                             unique=True)
     
     class Meta:
         abstract = True
