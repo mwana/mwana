e057feffa9f96ec4d1264b622f77e5697ff618e7
tsinkala <sinkalation@gmail.com>
2011-03-30 20:10:31 +0200

    only allowing dbs clinics in GroupFacilityMapping

--- mwana/apps/reports/webreports/models.py
+++ mwana/apps/reports/webreports/models.py
@@ -1,11 +1,10 @@
 # vim: ai ts=4 sts=4 et sw=4
+from django.contrib.auth.models import User
 from django.db import models
 from mwana.apps.locations.models import Location
-#from rapidsms.models import Connection
-from django.contrib.auth.models import User
 
 class ReportingGroup(models.Model):
-    name = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=100, unique=True)
 
     def __unicode__(self):
         return self.name
@@ -16,11 +15,12 @@ class GroupUserMapping(models.Model):
     group = models.ForeignKey(ReportingGroup)
 
     def __unicode__(self):
-        return "%s: %s"%(self.user,self.group)
+        return "%s: %s" % (self.user, self.group)
 
 class GroupFacilityMapping(models.Model):
     group = models.ForeignKey(User)
-    facilty = models.ForeignKey(Location)
+    facilty = models.ForeignKey(Location, limit_choices_to={
+                                "send_live_results":True})
 
     def __unicode__(self):
-        return "%s: %s"%(self.group,self.facilty)
+        return "%s: %s" % (self.group, self.facilty)
