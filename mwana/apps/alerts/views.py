159420905a088efe307d7779f467b8f7e70ec2cd
tsinkala <sinkalation@gmail.com>
2011-05-13 10:56:14 +0300

    adding filtering by user/location/implementer to alerts

--- mwana/apps/alerts/views.py
+++ mwana/apps/alerts/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.http import require_GET
 from mwana.apps.alerts.labresultsalerts.alerter import Alerter
+from mwana.apps.reports.views import get_facilities_dropdown_html
+from mwana.apps.reports.views import get_groups_dropdown_html
+from mwana.apps.reports.views import read_request
 
 def get_int(val):
     return int(val) if str(val).isdigit() else None
@@ -22,8 +25,22 @@ def mwana_alerts (request):
     lab_processing_days = get_from_request(request, 'input_lab_processing_days')
     lab_sending_days = get_from_request(request, 'input_lab_sending_days')
 
+    is_report_admin = False
+    try:
+        user_group_name = request.user.groupusermapping_set.all()[0].group.name
+        if request.user.groupusermapping_set.all()[0].group.id in (1,2)\
+        and ("moh" in user_group_name.lower() or "support" in user_group_name.lower()):
+            is_report_admin = True
+    except:
+        pass
 
-    alerter = Alerter()
+    rpt_group = read_request(request, "rpt_group")
+    rpt_provinces = read_request(request, "rpt_provinces")
+    rpt_districts = read_request(request, "rpt_districts")
+
+
+    alerter = Alerter(request.user,rpt_group,rpt_provinces,rpt_districts)
+
     transport_time, not_sending_dbs_alerts = \
         alerter.get_districts_not_sending_dbs_alerts(transport_time)
     retrieving_time, not_retrieving_results = \
@@ -55,5 +72,10 @@ def mwana_alerts (request):
                               'lab_sending_days':lab_sending_days,
 
                               'days':range(1, 60),
+                              'is_report_admin': is_report_admin,
+                              'region_selectable': True,
+                              'rpt_group': get_groups_dropdown_html('rpt_group',rpt_group),
+                              'rpt_provinces': get_facilities_dropdown_html("rpt_provinces", alerter.get_rpt_provinces(request.user), rpt_provinces) ,
+                              'rpt_districts': get_facilities_dropdown_html("rpt_districts", alerter.get_rpt_districts(request.user), rpt_districts) ,
                               }, context_instance=RequestContext(request)
                               )
