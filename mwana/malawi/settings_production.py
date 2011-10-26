8755539eba0a72e6566a7fbbdc1b1c43c94171b8
Lengani Kaunda <lengani@p4studio.com>
2011-10-26 15:31:40 +0200

    change tab name for Results160.

--- mwana/malawi/settings_production.py
+++ mwana/malawi/settings_production.py
@@ -33,7 +33,7 @@ RAPIDSMS_TABS = [
 #    ('rapidsms.contrib.messaging.views.messaging', 'Messaging'),
 #    ('rapidsms.contrib.scheduler.views.index', 'Event Scheduler'),
 #    ('mwana.apps.labresults.views.dashboard', 'Results160'),
-    ('mwana.apps.reports.views.malawi_reports', 'Reports'),
+    ('mwana.apps.reports.views.malawi_reports', 'Results160'),
     ('mwana.apps.alerts.views.mwana_alerts', 'Alerts'),
     ('growth_index', 'Growth Monitor'),
 #    ('xforms', 'XForms'),
