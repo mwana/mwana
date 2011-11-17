41f9bf9a80c2943306a80cf6a6fa8e32216ca560
tsinkala <sinkalation@gmail.com>
2011-11-17 15:42:03 +0200

    improving admin page for help requests:
    bug fixing-use 'requested_by__identity' instead of 'requested_by' if search_fields
    adding 'parent_loc', 'location', 'name', '[worker]type' to the fields displayed
    adding 'requested_by__contact__name', 'requested_by__contact__location__parent__name', 'requested_by__contact__location__parent__name', and 'requested_by__contact__types__name'
    to search_fields

--- mwana/apps/help/admin.py
+++ mwana/apps/help/admin.py
@@ -7,10 +7,38 @@ from mwana.apps.help import models as help
 
 
 class HelpRequestAdmin(admin.ModelAdmin):
-    list_display = ('requested_by', 'requested_on', 'additional_text','status',)
+    list_display = ('parent_loc', 'location', 'requested_by', 'name', 'type',
+    'requested_on', 'additional_text','status',)
     list_filter = ('requested_on',)
     list_select_related = True
-    search_fields = ('requested_by', 'status',)
+    search_fields = ('requested_by__identity', 'status', 'requested_by__contact__name',
+    'requested_by__contact__location__parent__name',
+    'requested_by__contact__location__parent__name',
+    'requested_by__contact__types__name',)
+
+    def type(self, obj):
+        try:
+            return ",".join(type.name for type in obj.requested_by.contact.types.all())
+        except:
+            return ""
+
+    def parent_loc(self, obj):
+        try:
+            return obj.requested_by.contact.location.parent.name
+        except:
+            return ""
+
+    def location(self, obj):
+        try:
+            return obj.requested_by.contact.location.name
+        except:
+            return ""
+
+    def name(self, obj):
+        try:
+            return obj.requested_by.contact.name
+        except:
+            return ""
 admin.site.register(help.HelpRequest, HelpRequestAdmin)
 
 
