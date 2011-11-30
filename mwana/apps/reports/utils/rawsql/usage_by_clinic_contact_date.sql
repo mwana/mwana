1890fff5345f77a6c563958ec32d36d6b7de3e85
tsinkala <sinkalation@gmail.com>
2011-11-30 17:34:46 +0200

    adding a field (contact type) to an adhoc SQL query

--- mwana/apps/reports/utils/rawsql/usage_by_clinic_contact_date.sql
+++ mwana/apps/reports/utils/rawsql/usage_by_clinic_contact_date.sql
@@ -1,9 +1,11 @@
-SELECT clinic,messagelog_message.text, phone,rapidsms_contact."name",messagelog_message.date from reports_messagegroup
+SELECT distinct clinic, contactsplus_contacttype."slug",  phone, rapidsms_contact."name", messagelog_message.text, messagelog_message.date from reports_messagegroup
 join messagelog_message on messagelog_message.id=reports_messagegroup.id
 --select messagelog_message.text,rapidsms_contact. * from messagelog_message
 join rapidsms_contact on rapidsms_contact.id=messagelog_message.contact_id
 join locations_location on locations_location.id = rapidsms_contact.location_id
 join locations_location parent on locations_location.parent_id = parent.id
+join rapidsms_contact_types on rapidsms_contact_types.contact_id = rapidsms_contact.id
+join contactsplus_contacttype on contactsplus_contacttype.id = rapidsms_contact_types.contacttype_id
 
 where messagelog_message.direction = 'I'
 and messagelog_message.date > '2010-06-13'
@@ -11,4 +13,4 @@ and parent.slug like '8080%'
 AND backEND <> 'message tester'
 and is_active = true
 
-order by clinic, phone, text
+order by clinic, phone, text, contactsplus_contacttype.slug
