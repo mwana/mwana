3105147f51ed4176f24d70f44b5f2c1683a482db
tsinkala <sinkalation@gmail.com>
2012-07-05 19:47:46 +0200

    marking messages for translation

--- mwana/apps/reports/tasks.py
+++ mwana/apps/reports/tasks.py
@@ -20,9 +20,11 @@ from rapidsms.messages import OutgoingMessage
 from rapidsms.models import Contact
 logger = logging.getLogger(__name__)
 
+_ = lambda s: s
+
 EID_BIRTH_SUMMARY = "%(name)s, %(month)s %(location_name)s EID & Birth Totals\nDBS Samples sent: %(samples)s ***\nDBS Results received: %(results)s ***\nBirths registered: %(births)s"
-CBA_THANKS_MSG = "Thank you, %(name)s. You have helped about %(helps)s mothers in your community in %(month)s %(year)s. Keep up the good work, reminding mothers saves lives."
-CBA_REMINDER_MSG = "Hello %(name)s. Remember to register births in your community and to remind mothers go to the clinic. Reminding mothers saves lives."
+CBA_THANKS_MSG = _("Thank you, %(name)s. You have helped about %(helps)s mothers in your community in %(month)s %(year)s. Keep up the good work, reminding mothers saves lives.")
+CBA_REMINDER_MSG = _("Hello %(name)s. Remember to register births in your community and to remind mothers go to the clinic. Reminding mothers saves lives.")
 
 def send_dho_eid_and_birth_report(router):
     logger.info('notifying district workers of monthly EID and Births summary')
@@ -151,7 +153,7 @@ def send_cba_birth_report(router):
         helps = births + traces
         if helps == 0:
             continue
-        
+
         msg = (CBA_THANKS_MSG % {'name':name, 'month':month,
                'year':last_year, 'helps':helps})
 
