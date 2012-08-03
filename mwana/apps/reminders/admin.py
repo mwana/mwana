c7ea89738ad4124771d76022e324dad0b28333b9
tsinkala <sinkalation@gmail.com>
2012-08-03 11:03:09 +0200

    minor. in patient event admin. adding search by cba_conn__identity

--- mwana/apps/reminders/admin.py
+++ mwana/apps/reminders/admin.py
@@ -31,7 +31,7 @@ class PatientEventAdmin(admin.ModelAdmin):
     'cba_conn','notification_status','notification_sent_date',)
     list_filter = ('event', 'event_location_type', 'date_logged','notification_status',)
     date_hierarchy = 'date_logged'
-    search_fields = ('patient__name', 'patient__location__parent__name',)
+    search_fields = ('patient__name', 'cba_conn__identity','patient__location__parent__name',)
 
     def clinic(self, obj):
         try:
