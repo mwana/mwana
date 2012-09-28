cfd01f002342b5f48ac3d816b990465ae0dfecc3
Cory Zue <czue@dimagi.com>
2012-09-28 11:48:08 -0400

    only try to send reminders to people with phone numbers

--- mwana/apps/smgl/reminders.py
+++ mwana/apps/smgl/reminders.py
@@ -33,12 +33,13 @@ def send_followup_reminders():
     for v in visits_to_remind:
         found_someone = False
         for c in v.mother.get_laycounselors():
-            found_someone = True
-            c.message(const.REMINDER_FU_DUE,
-                      **{"name": v.mother.name,
-                         "unique_id": v.mother.uid,
-                         "loc": v.location.name})
-            _create_notification("nvd", c, v.mother.uid)
+            if c.default_connection:
+                found_someone = True
+                c.message(const.REMINDER_FU_DUE,
+                          **{"name": v.mother.name,
+                             "unique_id": v.mother.uid,
+                             "loc": v.location.name})
+                _create_notification("nvd", c, v.mother.uid)
         if found_someone:
             v.reminded = True
             v.save()
@@ -68,12 +69,13 @@ def send_non_emergency_referral_reminders():
     for ref in referrals_to_remind:
         found_someone = False
         for c in ref.mother.get_laycounselors():
-            found_someone = True
-            c.message(const.REMINDER_NON_EMERGENCY_REFERRAL,
-                      **{"name": ref.mother.name,
-                         "unique_id": ref.mother.uid,
-                         "loc": ref.facility.name})
-            _create_notification("nem_ref", c, ref.mother.uid)
+            if c.default_connection:
+                found_someone = True
+                c.message(const.REMINDER_NON_EMERGENCY_REFERRAL,
+                          **{"name": ref.mother.name,
+                             "unique_id": ref.mother.uid,
+                             "loc": ref.facility.name})
+                _create_notification("nem_ref", c, ref.mother.uid)
         if found_someone:
             ref.reminded = True
             ref.save()
@@ -97,12 +99,13 @@ def send_emergency_referral_reminders():
     for ref in referrals_to_remind:
         found_someone = False
         for c in ref.get_receiving_data_clerks():
-            found_someone = True
-            c.message(const.REMINDER_EMERGENCY_REFERRAL,
-                      **{"unique_id": ref.mother_uid,
-                         "date": ref.date.date(),
-                         "loc": ref.referring_facility.name if ref.referring_facility else "?"})
-            _create_notification("em_ref", c, ref.mother_uid)
+            if c.default_connection:
+                found_someone = True
+                c.message(const.REMINDER_EMERGENCY_REFERRAL,
+                          **{"unique_id": ref.mother_uid,
+                             "date": ref.date.date(),
+                             "loc": ref.referring_facility.name if ref.referring_facility else "?"})
+                _create_notification("em_ref", c, ref.mother_uid)
         if found_someone:
             ref.reminded = True
             ref.save()
@@ -129,12 +132,13 @@ def send_upcoming_delivery_reminders():
     for mom in moms_to_remind:
         found_someone = False
         for c in mom.get_laycounselors():
-            found_someone = True
-            c.message(const.REMINDER_UPCOMING_DELIVERY,
-                      **{"name": mom.name,
-                         "unique_id": mom.uid,
-                         "date": mom.edd})
-            _create_notification("edd_14", c, mom.uid)
+            if c.default_connection:
+                found_someone = True
+                c.message(const.REMINDER_UPCOMING_DELIVERY,
+                          **{"name": mom.name,
+                             "unique_id": mom.uid,
+                             "date": mom.edd})
+                _create_notification("edd_14", c, mom.uid)
         if found_someone:
             mom.reminded = True
             mom.save()
