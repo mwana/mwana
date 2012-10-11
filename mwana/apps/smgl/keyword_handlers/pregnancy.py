9eb0372ba1a2a717a0e62577e6874b47d0421b90
David Ray <davidray@gmail.com>
2012-10-11 10:36:32 -0400

    PEP8 cleanup

--- mwana/apps/smgl/keyword_handlers/pregnancy.py
+++ mwana/apps/smgl/keyword_handlers/pregnancy.py
@@ -12,6 +12,7 @@ from mwana.apps.smgl.decorators import registration_required
 
 logger = logging.getLogger(__name__)
 
+
 @registration_required
 def pregnant_registration(session, xform, router):
     """
@@ -23,42 +24,39 @@ def pregnant_registration(session, xform, router):
     logger.debug('Handling the REG keyword form')
 
     connection = session.connection
-    
-    # We must get the location from the Contact (Data Associate) who 
-    # should be registered with this phone number.  If the contact does 
+
+    # We must get the location from the Contact (Data Associate) who
+    # should be registered with this phone number.  If the contact does
     # not exist (unregistered) we throw an error.
-    contactType = ContactType.objects.get(slug__iexact=const.CTYPE_DATACLERK) #da = Data Associate
+    contactType = ContactType.objects.get(slug__iexact=const.CTYPE_DATACLERK)  # da = Data Associate
     try:
         data_associate = Contact.objects.get(connection=connection, types=contactType)
     except ObjectDoesNotExist:
         send_msg(connection, const.NOT_REGISTERED_FOR_DATA_ASSOC, router)
         return True
 
-
-    # get or create a new Mother Object without saving the object 
+    # get or create a new Mother Object without saving the object
     # (and triggering premature errors)
-    
     uid = get_value_from_form('unique_id', xform)
     if PregnantMother.objects.filter(uid=uid).count():
-        send_msg(connection, const.DUPLICATE_REGISTRATION, router,  
+        send_msg(connection, const.DUPLICATE_REGISTRATION, router,
                  **{"unique_id": uid})
         return True
 
     mother = PregnantMother(uid=uid)
     mother.created_date = session.modified_time
-    
-    
+
     mother.first_name = get_value_from_form('first_name', xform)
     mother.last_name = get_value_from_form('last_name', xform)
-    
+
     next_visit, error_msg = make_date(xform, "next_visit_dd", "next_visit_mm", "next_visit_yy")
     if error_msg:
         send_msg(connection, error_msg, router, **{"date_name": "Next Visit",
                                                    "error_msg": error_msg})
         return True
-    
+
     if next_visit < datetime.datetime.now().date():
-        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,  
+        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,
                  **{"date_name": "Next Visit", "date": next_visit})
         return True
 
@@ -73,7 +71,7 @@ def pregnant_registration(session, xform, router):
         return True
 
     if lmp_date and lmp_date > datetime.datetime.now().date():
-        send_msg(connection, const.DATE_MUST_BE_IN_PAST, router,  
+        send_msg(connection, const.DATE_MUST_BE_IN_PAST, router,
                  **{"date_name": "LMP", "date": lmp_date})
         return True
 
@@ -83,9 +81,9 @@ def pregnant_registration(session, xform, router):
         send_msg(connection, error_msg, router, **{"date_name": "EDD",
                                                    "error_msg": error_msg})
         return True
-    
+
     if edd_date and edd_date < datetime.datetime.now().date():
-        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,  
+        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,
                  **{"date_name": "EDD", "date": edd_date})
         return True
 
@@ -105,22 +103,22 @@ def pregnant_registration(session, xform, router):
     if reasons:
         for r in reasons.split(" "):
             mother.set_risk_reason(r)
-    
+
     mother.contact = data_associate
     mother.save()
 
-    send_msg(connection, const.MOTHER_SUCCESS_REGISTERED, router, 
+    send_msg(connection, const.MOTHER_SUCCESS_REGISTERED, router,
              **{"name": mother.contact.name, "unique_id": mother.uid})
-    
+
     # if there is a lay counselor(s) registered, also notify them
     for contact in mother.get_laycounselors():
         if contact.default_connection:
-            send_msg(contact.default_connection, 
-                     const.NEW_MOTHER_NOTIFICATION, router, 
+            send_msg(contact.default_connection,
+                     const.NEW_MOTHER_NOTIFICATION, router,
                      **{"mother": mother.name, "unique_id": mother.uid})
-        
 
     return True
+
 
 @registration_required
 def follow_up(session, xform, router):
@@ -134,7 +132,7 @@ def follow_up(session, xform, router):
     except ObjectDoesNotExist:
         send_msg(connection, const.NOT_A_DATA_ASSOCIATE, router, **session.template_vars)
         return True
-    
+
     unique_id = get_value_from_form('unique_id', xform)
     try:
         mother = PregnantMother.objects.get(uid=unique_id)
@@ -147,27 +145,25 @@ def follow_up(session, xform, router):
         send_msg(connection, error_msg, router, **{"date_name": "EDD",
                                                    "error_msg": error_msg})
         return True
-    
+
     if edd_date and edd_date < datetime.datetime.now().date():
-        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,  
+        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,
                  **{"date_name": "EDD", "date": edd_date})
         return True
 
-    
     visit_reason = get_value_from_form('visit_reason', xform)
     next_visit, error_msg = make_date(xform, "next_visit_dd", "next_visit_mm", "next_visit_yy")
     if error_msg:
         send_msg(connection, error_msg, router, **{"date_name": "Next Visit",
                                                    "error_msg": error_msg})
-        
         return True
-    
+
     if next_visit < datetime.datetime.now().date():
-        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,  
+        send_msg(connection, const.DATE_MUST_BE_IN_FUTURE, router,
                  **{"date_name": "Next Visit", "date": next_visit})
         return True
 
-    # Make the follow up facility visit 
+    # Make the follow up facility visit
     visit = FacilityVisit()
     visit.mother = mother
     visit.contact = contact
@@ -178,17 +174,18 @@ def follow_up(session, xform, router):
     visit.visit_date = datetime.datetime.utcnow().date()
     visit.created_date = session.modified_time
     visit.save()
-    
+
     send_msg(connection, const.FOLLOW_UP_COMPLETE, router, name=contact.name, unique_id=mother.uid)
+
 
 @registration_required
 def told(session, xform, router):
     # TODO: processing, if necessary
     if not session.connection.contact:
-        send_msg(session.connection, const.NOT_REGISTERED_FOR_DATA_ASSOC, router, 
+        send_msg(session.connection, const.NOT_REGISTERED_FOR_DATA_ASSOC, router,
                  name=session.connection.contact.name)
         return True
-    
-    send_msg(session.connection, const.TOLD_COMPLETE, router, 
+
+    send_msg(session.connection, const.TOLD_COMPLETE, router,
              name=session.connection.contact.name)
     return True
