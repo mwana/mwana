4c19c8e19c97805c83e023ac7046820fbc4e5cfd
tsinkala <sinkalation@gmail.com>
2012-10-31 18:46:08 +0200

    fixing bemba translations

--- mwana/apps/patienttracing/tasks.py
+++ mwana/apps/patienttracing/tasks.py
@@ -18,7 +18,7 @@ _ = lambda s: s
 
 def send_confirmation_reminder(router):
     logger.info("Retrieving PatientTraces for confirmation reminder messages")
-    confirmation_reminder_txt = _("Hi %s! If you know that %s has been to the clinic, please send: CONFIRM %s")
+    confirmation_reminder_txt = _("Hi %(cba)s! If you know that %(patient)s has been to the clinic, please send: CONFIRM %(name)s")
     DAYS_AFTER_TOLD = 3
     confirm_window_date = (datetime.now() - timedelta(days=DAYS_AFTER_TOLD))
     traces = PatientTrace.objects.filter(status=patienttracing.get_status_told())\
@@ -33,7 +33,7 @@ def send_confirmation_reminder(router):
             logger.info ("Trace (%s) does not have a usable Connection!" % (str(PatientTrace)))
             continue #Can't send confirmation message if we have no one to send it to.
         connection = trace.messenger.default_connection
-        message_txt = confirmation_reminder_txt % (trace.messenger.name, trace.name,trace.name)
+        message_txt = confirmation_reminder_txt % {'cba':trace.messenger.name, 'patient':trace.name, 'name':trace.name}
         msg = OutgoingMessage(connection, message_txt)
         msg.send()
         scm = SentConfirmationMessage.objects.create(patient_name=trace.name, \
