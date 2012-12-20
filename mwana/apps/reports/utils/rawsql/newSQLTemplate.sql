0783b8f57d775f71761f18f2e030d669fa992eae
tsinkala <sinkalation@gmail.com>
2012-12-20 14:32:17 +0200

    misc/minor changes to adhoc sql files

--- mwana/apps/reports/utils/rawsql/newSQLTemplate.sql
+++ mwana/apps/reports/utils/rawsql/newSQLTemplate.sql
@@ -1,7 +1,19 @@
-SELECT parent."name", locations_location."name", min(result_sent_date) from labresults_result
-join locations_location on locations_location.id=labresults_result.clinic_id
-join locations_location parent on parent.id = locations_location.parent_id
-where parent."name" in ('Mazabuka','Monze')
 
-group by parent."name", locations_location."name"
-order by parent."name", locations_location."name"
+SELECT parent."name" as "District", locations_location."name" as "Clinic",
+ rapidsms_contact."name", contactsplus_contacttype."name" ,
+rapidsms_connection."identity" as "Phone",messagelog_message.direction, messagelog_message.date, text  from messagelog_message
+join rapidsms_contact on rapidsms_contact.id = messagelog_message.contact_id
+join locations_location on locations_location.id = rapidsms_contact.location_id
+join rapidsms_connection on rapidsms_connection.id = messagelog_message.connection_id
+--select * FROM locations_location
+left join locations_location as parent on parent.id = locations_location.parent_id
+join rapidsms_contact_types on rapidsms_contact_types.contact_id = rapidsms_contact.id
+join contactsplus_contacttype on contactsplus_contacttype.id = rapidsms_contact_types.contacttype_id
+--where parent.slug in ('808030', '808014', '808021', '808015', '808025', '807035', '807026', '807033', '807037', '807099', '804011', '804012', '804013', '804001', '804031', '804028', '804022', '804034', '804016', '804002', '804030', '804024')
+where locations_location.slug in ('800000', '800000', '800000')--, '808015', '808025', '807035', '807026', '807033', '807037', '807099', '804011', '804012', '804013', '804001', '804031', '804028', '804022', '804034', '804016', '804002', '804030', '804024')
+--and messagelog_message.direction ='I'
+and messagelog_message.date between '20110101' and '20121001'
+order by 1,2,3,"Phone", date
+
+--SELECT * from contactsplus_contacttype
+
--- mwana/apps/reports/utils/rawsql/users_by_type_and_facility.sql
+++ mwana/apps/reports/utils/rawsql/users_by_type_and_facility.sql
@@ -1,38 +1,12 @@
-SELECT DISTINCT
-contactsplus_contacttype.slug,
-CASE
-WHEN locations_locationtype.sINgular ilike 'zone' then great_grANDy.name
-WHEN locations_locationtype.sINgular ilike 'district' then parent.name
-WHEN locations_locationtype.sINgular ilike 'provINce' then location.name
-else grANDy."name"
-END as provINce,
-CASE
-WHEN locations_locationtype.sINgular ilike 'zone' then grANDy.name
-WHEN locations_locationtype.sINgular ilike 'district' then location.name
-WHEN locations_locationtype.sINgular ilike 'provINce' then null
-else parent."name"
-END as district,
-CASE
-WHEN locations_locationtype.sINgular ilike 'zone' then parent.name
-else location."name"
-END as facility,
-
-count(*)
-FROM rapidsms_contact
+SELECT contactsplus_contacttype.name,count(*) from rapidsms_contact
+LEFT JOIN rapidsms_contact_types ON rapidsms_contact_types.contact_id=rapidsms_contact.id
+LEFT JOIN contactsplus_contacttype ON contactsplus_contacttype.id=rapidsms_contact_types.contacttype_id
 LEFT JOIN rapidsms_connection ON rapidsms_connection.contact_id=rapidsms_contact.id
 LEFT JOIN locations_location location ON rapidsms_contact.location_id=location.id
 LEFT JOIN locations_location parent ON location.parent_id=parent.id
-LEFT JOIN locations_location child ON child.parent_id=location.id
-LEFT JOIN locations_location grANDy ON grANDy.id=parent.parent_id
-LEFT JOIN locations_location great_grANDy ON great_grANDy.id=grANDy.parent_id
-LEFT JOIN locations_locationtype ON locations_locationtype.id=location.type_id
-LEFT JOIN rapidsms_contact_types ON rapidsms_contact_types.contact_id=rapidsms_contact.id
-LEFT JOIN contactsplus_contacttype ON contactsplus_contacttype.id=rapidsms_contact_types.contacttype_id
-WHERE  is_active = 't'
+WHERE is_active='t'
+and contactsplus_contacttype.name <> 'Patient'
 AND rapidsms_connection.identity like '+260%'
-AND location.name NOT ilike 'support'
---AND locations_locationtype.sINgular ilike 'zone'
-GROUP BY contactsplus_contacttype.slug, facility, provINce, district
-ORDER BY 1,2,3,4
-
+and (location.slug like '80%' or parent.slug like '80%')
+group by contactsplus_contacttype.name
 
