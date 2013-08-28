9dbb35224e7ba5f3e19fdef389235aa679c56663
tsinkala <sinkalation@gmail.com>
2013-08-28 14:45:23 +0200

    updating script for data cleanup

--- mwana/apps/patienttracing/management/commands/correct_automated_traces.py
+++ mwana/apps/patienttracing/management/commands/correct_automated_traces.py
@@ -4,12 +4,14 @@ the system but were not subsequently updated due to a bug that has since been
 fixed
 """
 from datetime import timedelta
+import re
 
 from django.core.management.base import LabelCommand
+from django.db.models import Q
 from mwana.apps.patienttracing.models import CorrectedTrace
 from mwana.apps.patienttracing.models import PatientTrace
+from mwana.apps.reminders.models import PatientEvent
 from mwana.util import get_clinic_or_default
-
 
 class Command(LabelCommand):
     help = """Corrects PatientTrace objects that were system initiated"""
@@ -36,7 +38,6 @@ def correct_misspelt_status():
 
 
 def correct_tolds():
-    pt = PatientTrace()
     count = 0
     for pt in PatientTrace.objects.filter(initiator='cba', status='told', patient_event=None):
         messenger = pt.messenger
@@ -62,7 +63,6 @@ def correct_tolds():
 
 
 def correct_confirms():
-    pt = PatientTrace()
     count = 0
     for pt in PatientTrace.objects.filter(initiator='cba', status='confirmed',
                                           patient_event=None).exclude(reminded_date=None):
@@ -97,7 +97,67 @@ def cleanup():
     # TODO: clear CBA initiated traces that have been mapped to system initiated ones
     pass
 
+def clean_names():
+    count = 0
+    # clean patient event's name
+    for pe in PatientEvent.objects.filter(patient__name__iregex='\d\d .*'):
+        patient = pe.patient
+        name = re.sub(r"\d\d", "", patient.name).lower().strip()
+        name = re.sub(r"\s+", " ", name)
+
+        if name.startswith("f "):
+            pe.event_location_type = 'cl'
+            pe.save()
+            name = name[2:]
+        elif name.startswith("h "):
+            pe.event_location_type = 'hm'
+            pe.save()
+            name = name[2:]
+
+        name = name.strip().title()
+        patient.name = name
+        patient.save()
+        count += 1
+
+    # clean name in patient trace
+    for pt in PatientTrace.objects.filter(name__iregex='\d\d .*'):
+        name = re.sub(r"\d\d", "", pt.name).lower().strip().title()
+        name = re.sub(r"\s+", " ", name)
+        pt.name = name
+        pt.save()
+
+    for pt in PatientTrace.objects.filter(name__istartswith='f '):
+        pt.name = pt.name[2:].strip().title()
+        pt.save()
+        count += 1
+
+    for pt in PatientTrace.objects.filter(name__istartswith='h '):
+        pt.name = pt.name[2:].strip().title()
+        pt.save()
+        count += 1
+
+    for pt in PatientTrace.objects.filter(name__istartswith='mwana '):
+        pt.name = pt.name[6:].strip().title()
+        pt.save()
+        count += 1
+
+    for pt in PatientTrace.objects.filter(Q(name__contains='6'),
+                                          Q(name__icontains='day') |
+                                          Q(name__icontains='week') |
+                                          Q(name__icontains='month')):
+        name = pt.name.strip().lower()
+        for item in ['6days', '6 days', '6weeks', '6 weeks', '6months', '6 months',
+            "6day", "6 day", "6week", "6 week", "6month", "6 month"]:
+            name = name.replace(item, "")
+
+        pt.name = name.strip().title()
+        pt.save()
+        count += 1
+
+    print "Made %s corrections to names" % count
+
 def correct_patient_traces():
+    clean_names()
     correct_missing_clinic()
     correct_misspelt_status()
     correct_tolds()
