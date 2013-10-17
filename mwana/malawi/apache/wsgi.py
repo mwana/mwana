19a14471031c38f6fb635cd4d8e39494ac835d8c
Lengani Kaunda <lengani@gmail.com>
2013-10-17 16:35:03 +0200

    changes for new wsgi.py

--- mwana/malawi/apache/wsgi.py
+++ mwana/malawi/apache/wsgi.py
@@ -13,7 +13,7 @@ CODE_ROOT = dirname(MWANA_DIR)
 DEPLOY_ROOT = dirname(CODE_ROOT)
 VIRTUALENV_ROOT = os.path.join(DEPLOY_ROOT, 'python_env')
 
-site_dir = os.path.join(VIRTUALENV_ROOT, 'lib', 'python2.6', 'site-packages')
+site_dir = os.path.join(VIRTUALENV_ROOT, 'lib', 'python2.7', 'site-packages')
 site.addsitedir(site_dir)
 sys.path.insert(0, CODE_ROOT)
 
@@ -32,9 +32,6 @@ try:
                       settings.LOG_BACKUPS, settings.LOG_LEVEL,
                       settings.LOG_FORMAT)
     from django.core.wsgi import get_wsgi_application
-    # import djcelery
-    # Djcelery is already imported in settings
-    # djcelery.setup_loader()
 
     django_app = get_wsgi_application()
 except:
@@ -42,34 +39,38 @@ except:
     traceback.print_exc(file=sys.stderr)
     django_app = None
 
+
 def static_response(environ, start_response, status, file, default_message=''):
-    response_headers = [('Retry-After', '120')] # Retry-After: <seconds>
+    response_headers = [('Retry-After', '120')]  # Retry-After: <seconds>
     if os.path.exists(file):
-        response_headers.append(('Content-type','text/html'))
+        response_headers.append(('Content-type', 'text/html'))
         response = open(file).read()
     else:
-        response_headers.append(('Content-type','text/plain'))
+        response_headers.append(('Content-type', 'text/plain'))
         response = default_message
     start_response(status, response_headers)
     return [response]
+
 
 def server_error(environ, start_response):
     status = '500 Internal Server Error'
     msg = 'Internal Server Error...please retry in a few minutes.'
     return static_response(environ, start_response, status, ERROR_FILE, msg)
 
+
 def upgrade_in_progress(environ, start_response):
     if environ['REMOTE_ADDR'] in ADMIN_IPS and django_app:
         return django_app(environ, start_response)
-    
+
     if environ['REQUEST_METHOD'] == 'GET':
         status = '503 Service Unavailable'
     else:
         status = '405 Method Not Allowed'
-    
+
     msg = 'Upgrade in progress...please retry in a few minutes.'
     return static_response(environ, start_response, status, UPGRADE_FILE, msg)
-    
+
+
 class LoggingMiddleware(object):
     def __init__(self, application):
         self.__application = application
