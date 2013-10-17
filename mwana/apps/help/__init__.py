bc83f1836d920afdab252c0de82ae017c1eeaef3
Lengani Kaunda <lengani@gmail.com>
2013-10-17 17:16:31 +0200

    avoid import issues

--- mwana/apps/help/__init__.py
+++ mwana/apps/help/__init__.py
@@ -1,3 +1,1 @@
 # vim: ai ts=4 sts=4 et sw=4
-from .handlers.facilitycode import CodeHandler
-from .handlers.facilitycontacts import ContactsHandler
--- mwana/apps/labresults/__init__.py
+++ mwana/apps/labresults/__init__.py
@@ -1,6 +1,1 @@
 # vim: ai ts=4 sts=4 et sw=4
-# from .handlers.join import JoinHandler
-from .handlers.leave import UnregisterHandler
-# from .handlers.printer import PrinterHandler
-# from .handlers.results import ResultsHandler
-# from .handlers.sent import SentHandler
