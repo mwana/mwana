49c81a9c0b7db81f6eaef8fa1ee3e3bd82c8a17f
Andre Lesa <andre@andrelesa.com>
2013-10-31 06:41:06 +0200

    User name on message panel is now a link.

--- mwana/apps/smgl/tables.py
+++ mwana/apps/smgl/tables.py
@@ -132,7 +132,7 @@ class SummaryReportTable(Table):
 
 class SMSRecordsTable(Table):
     date = DateColumn(format="Y m d H:i")
-    user_name = NamedColumn(col_name="User Name", value= lambda cell: cell.object.connection.contact.name.title())
+    user_name = NamedColumn(link=lambda cell: reverse("sms-user-history", args=[cell.object.connection.contact.id]), col_name="User Name", value= lambda cell: cell.object.connection.contact.name.title())
     msg_type = NamedColumn(col_name="Type",
                       value=lambda cell: cell.object.text.split(' ')[0].upper(),
                       sortable=False
