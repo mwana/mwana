e9898f19f63409eafa3848281d2d0a3440aed265
Andre Lesa <andre@andrelesa.com>
2013-11-04 20:14:48 +0200

    returned user phone number to message records table.

--- mwana/apps/smgl/tables.py
+++ mwana/apps/smgl/tables.py
@@ -132,6 +132,7 @@ class SummaryReportTable(Table):
 
 class SMSRecordsTable(Table):
     date = DateColumn(format="Y m d H:i")
+    phone_number = NamedColumn(col_name="Phone Number", value= lambda cell: cell.object.connection.identity)
     user_name = NamedColumn(link=lambda cell: reverse("sms-user-history", args=[cell.object.connection.contact.id]), col_name="User Name", value= lambda cell: cell.object.connection.contact.name.title())
     msg_type = NamedColumn(col_name="Type",
                       value=lambda cell: cell.object.text.split(' ')[0].upper(),
