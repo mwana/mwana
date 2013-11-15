94944af3b72a6c2e16961e46fc1f0d54b8b8c3cf
Andre Lesa <andre@andrelesa.com>
2013-11-15 05:24:23 +0200

    added anc_delivery report view.

--- mwana/apps/smgl/views.py
+++ mwana/apps/smgl/views.py
@@ -40,6 +40,200 @@ def fetch_initial(initial, session):
 
 def save_form_data(cleaned_data, session):
     session['form_data'] = cleaned_data
+    
+def anc_delivery_report(request):  
+    records = []
+    facility_parent = None
+    start_date, end_date = get_default_dates()
+
+    province = district = facility = None
+
+    visits = FacilityVisit.objects.all()
+    records_for = Location.objects.filter(type__singular='district')
+    start_date, end_date = get_default_dates()
+    if id:
+        facility_parent = get_object_or_404(Location, id=id)
+        # Prepopulate the district
+        if not request.GET:
+            update = {u'district_0': facility_parent.name,
+                      u'district_1': facility_parent.id,
+                      u'start_date': start_date,
+                      u'end_date': end_date
+                      }
+            request.GET = request.GET.copy()
+            request.GET.update(update)
+
+    if request.GET:
+        form = StatisticsFilterForm(request.GET)
+        if form.is_valid():
+            save_form_data(form.cleaned_data, request.session)
+            province = form.cleaned_data.get('province')
+            district = form.cleaned_data.get('district')
+            facility = form.cleaned_data.get('facility')
+            start_date = form.cleaned_data.get('start_date', start_date)
+            end_date = form.cleaned_data.get('end_date', end_date)
+            filter_option = form.cleaned_data.get('date_filter_option')
+        # determine what location(s) to include in the report
+        if id:
+            # get district facilities
+            records_for = get_location_tree_nodes(facility_parent, None)
+            if district:
+                records_for = get_location_tree_nodes(district, None, ~Q(type__slug='zone'))
+                if facility_parent != district:
+                    redirect_url = reverse('district-stats',
+                                            args=[district.id])
+                    params = urllib.urlencode(request.GET)
+                    full_redirect_url = '%s?%s' % (redirect_url, params)
+                    return HttpResponseRedirect(full_redirect_url)
+            if facility:
+                records_for = [facility]
+        else:
+            if province:
+                records_for = Location.objects.filter(parent=province)
+            if district:
+                records_for = [district]
+    else:
+        initial = {
+                    'start_date': start_date,
+                    'end_date': end_date,
+                  }
+        form = StatisticsFilterForm(initial=fetch_initial(initial, request.session))
+
+    for place in records_for:
+        locations = Location.objects.all()
+        if not id:
+            reg_filter = {'district': place}
+            visit_filter = {'location__in': [x for x in locations \
+                                if get_current_district(x) == place]}
+        else:
+            reg_filter = {'location': place}
+            visit_filter = {'location': place}
+
+        # Get PregnantMother count for each place
+        if not id:
+            district_facilities = [x for x in locations \
+                                if get_current_district(x) == place]
+            pregnancies = PregnantMother.objects \
+                            .filter(location__in=district_facilities)
+        else:
+            pregnancies = PregnantMother.objects.filter(location=place)
+
+        if filter_option == 'option_1':
+            # Option 1 should show All women who have Pregnancy registrations AND gave birth OR had EDD scheduled within our 
+            #specified time frame
+            mothers_reg = filter_by_dates(mothers, 'created_date',
+                             start=start_date, end=end_date)
+            births = filter_by_dates(BirthRegistration.objects.all(), 'date',
+                            start=start_date, end=end_date)
+            #Get all mothers with birth registrations within the specified time frame.
+            mothers_births = mothers_reg.filter(id__in=births.values_list('mother'))
+            #Get all mothers with edd within the specified time frame
+            mothers_edd = filter_by_dates(mothers, 'edd',
+                             start=start_date, end=end_date)
+            #Combine the two querysets
+            mothers = mothers_births | mothers_edd
+        else:
+            # All women who gave birth or had EDD within specified time frame 
+            mothers_edd = filter_by_dates(mothers, 'edd',
+                             start=start_date, end=end_date)
+            
+            births = filter_by_dates(BirthRegistration.objects.all(), 'date',
+                            start=start_date, end=end_date)
+            #Get all mothers with birth registrations within the specified time frame.
+            mothers_births = mothers.filter(id__in=births.values_list('mother'))
+            mothers = mothers_edd | mothers_births
+     
+        r = {'location': place.name}
+
+        r['location_id'] = place.id
+        births = BirthRegistration.objects.filter(**reg_filter)
+
+        # utilize start/end date if supplied
+        births = filter_by_dates(births, 'date',
+                                 start=start_date, end=end_date)
+        r['births_com'] = births.filter(place='h').count()
+        r['births_fac'] = births.filter(place='f').count()
+
+
+
+        # Aggregate ANC visits by Mother and # of visits
+        place_visits = visits.filter(**visit_filter)
+        place_visits = filter_by_dates(place_visits, 'visit_date',
+                                  start=start_date, end=end_date)
+
+        mother_ids = place_visits.distinct('mother') \
+                            .values_list('mother', flat=True)
+        mothers = PregnantMother.objects.filter(id__in=mother_ids)
+
+        anc_visits = mothers.filter(facility_visits__visit_type='anc') \
+                            .annotate(Count('facility_visits')) \
+                            .values_list('facility_visits__count', flat=True)
+
+        r['anc1'] = r['anc2'] = r['anc3'] = r['anc4'] = 0
+        # ANC1 is PregnantMother registrations
+        r['pregnancies'] = pregnancies.count()
+        r['anc1'] = pregnancies.count()
+        num_visits = {}
+        for num in anc_visits:
+            if num in num_visits:
+                num_visits[num] += 1
+            else:
+                num_visits[num] = 1
+        for i in range(1, 4):
+            key = 'anc{0}'.format(i + 1)
+            if i in num_visits:
+                r[key] = num_visits[i]
+
+    # handle sorting, since djtables won't sort on non-Model based tables.
+    records = sorted(records, key=itemgetter('location'))
+    if request.GET.get('order_by'):
+        sort = False
+        attr = request.GET.get('order_by')
+        if attr.startswith('-'):
+            sort = True
+        attr = attr.strip('-')
+        try:
+            records = sorted(records, key=itemgetter(attr))
+        except KeyError:
+            pass
+        if sort:
+            records.reverse()
+
+    # render as CSV if export
+    if form.data.get('export'):
+        # expunge location_id field
+        [x.pop('location_id') for x in records]
+        # The keys must be ordered for the exporter
+        keys = ['location', 'pregnancies', 'births_com',
+                'births_fac', 'births_total',
+                'infant_deaths_com', 'infant_deaths_fac',
+                'infant_deaths_total', 'mother_deaths_com',
+                'mother_deaths_fac', 'mother_deaths_total', 'anc1', 'anc2',
+                'anc3', 'anc4', 'pos1', 'pos2', 'pos3']
+        filename = 'national_statistics' if not id else 'district_statistics'
+        date_range = ''
+        if start_date:
+            date_range = '_from{0}'.format(start_date)
+        if start_date:
+            date_range = '{0}_to{1}'.format(date_range, end_date)
+        filename = '{0}{1}'.format(filename, date_range)
+        return export_as_csv(records, keys, filename)
+
+    statistics_table = StatisticsLinkTable(records,
+                                           request=request)
+    # disable the link column if on district stats view
+    if id:
+        statistics_table = StatisticsTable(records,
+                                           request=request)
+    return render_to_response(
+        "smgl/statistics.html",
+        {"statistics_table": statistics_table,
+         "district": facility_parent,
+         "form": form
+        },
+        context_instance=RequestContext(request))
+
+    
 
 def mothers(request):
     province = district = facility = zone = None
