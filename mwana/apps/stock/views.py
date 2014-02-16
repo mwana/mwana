e675d382611614f94c01a259b4ff4fd8e76e22d8
tsinkala <sinkalation@gmail.com>
2014-02-16 21:58:50 +0200

    UI tweaks

--- mwana/apps/stock/views.py
+++ mwana/apps/stock/views.py
@@ -9,7 +9,6 @@ from mwana.apps.reports.utils.htmlhelper import get_facilities_dropdown_html
 
 from mwana.apps.reports.utils.facilityfilter import user_facilities
 
-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mwana.apps.reports.views import read_request
@@ -27,13 +26,7 @@ class TransactedStock:
 
 def stock(request):
     today = date.today()
-    try:
-        update_user_incidents = request.POST['update_user_incidents']
-        if update_user_incidents and update_user_incidents == 'True':
-            assign_new_indicators(request.user, map(int, dict(request.POST)["_select_case"]))
-    except KeyError:
-        pass
-
+    
     startdate1 = read_date_or_default(request, 'startdate', today - timedelta(days=30))
     enddate1 = read_date_or_default(request, 'enddate', today)
     start_date = min(startdate1, enddate1, date.today())
@@ -42,6 +35,12 @@ def stock(request):
     facilities = user_facilities(current_user=request.user, group=None, province=None, district=None, facility=None)
     
 #    rpt_provinces = read_request(request, "rpt_provinces")
+    selected_stock = None
+    try:
+        selected_stock = Stock.objects.filter(id__in=map(int, dict(request.POST).get("_select_stock", ['-1'])))
+    except KeyError, e:
+        logger.warn("KeyError: %s" % e)
+
     rpt_districts = read_request(request, "rpt_districts")
     stocks = Stock.objects.all()
     stockAccounts = StockAccount.objects.filter(location__in=facilities)
@@ -73,7 +72,7 @@ def stock(request):
                               "fend_date": end_date.strftime("%Y-%m-%d"),
                               "fstart_date2": start_date.strftime("%d %b %Y"),
                               "fend_date2": end_date.strftime("%d %b %Y"),
-                              "stock_selection_grid": get_stock_selector_grid("stock_selection_grid", Stock.objects.all().order_by("name"), None, 1),
+                              "stock_selection_grid": get_stock_selector_grid("stock_selection_grid", Stock.objects.all().order_by("name"), selected_stock, 1),
 #                              'rpt_provinces': get_facilities_dropdown_html("rpt_provinces", get_rpt_provinces(request.user), rpt_provinces),
                               'rpt_districts': get_facilities_dropdown_html("rpt_districts", get_rpt_districts(request.user), rpt_districts).replace("All", "------------"),
                               "facilities": facilities,
