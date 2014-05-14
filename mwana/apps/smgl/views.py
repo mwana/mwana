7dd74657d74d7f3420e613836872271adb70b346
Andre Lesa <andre@andrelesa.com>
2014-05-14 17:19:45 +0200

    MotherID export from server.

--- mwana/apps/smgl/views.py
+++ mwana/apps/smgl/views.py
@@ -248,6 +248,20 @@ def anc_report(request, id=None):
             if len(anc_visits) >= 4:
                 four_anc += 1
 
+        workbook = xlwt.Workbook(encoding='utf-8')
+        worksheet = workbook.add_sheet("Report Export")
+        row_index = 0
+        bold_style = xlwt.easyxf('font: bold 1')
+        worksheet.write(row_index, 0, 'Mother UID', bold_style)
+        row_index += 1
+        for mother in mothers:
+            worksheet.write(row_index, 0, mother.uid)
+            row_index += 1
+
+        now = datetime.datetime.now()
+        fname = 'reports_exports/ANC_report_%s_%s.xls'%(now.strftime("%d_%b_(%H_%M)"), filter_option)
+        workbook.save(fname)
+
 
         r['anc2'] = two_anc
         r['anc3'] = three_anc
@@ -434,6 +448,26 @@ def pnc_report(request, id=None):
 
             return (six_hour_pnc_num, six_day_pnc_num, six_week_pnc_num,
                 complete_pnc_num)
+
+
+        #!!!Export
+        workbook = xlwt.Workbook(encoding='utf-8')
+        worksheet = workbook.add_sheet("Report Export")
+        row_index = 0
+        bold_style = xlwt.easyxf('font: bold 1')
+        worksheet.write(row_index, 0, 'Mother UID', bold_style)
+        row_index += 1
+        for birth in births:
+            worksheet.write(row_index, 0, birth.mother.uid)
+            row_index += 1
+
+        now = datetime.datetime.now()
+        fname = 'reports_exports/PNC_report_%s.xls'%(now.strftime("%d_%b_(%H_%M)"))
+        workbook.save(fname)
+
+
+
+
 
         r['six_hour_pnc'], r['six_day_pnc'], r['six_week_pnc'],\
         r['complete_pnc'] = pnc_visits(births, visits)
