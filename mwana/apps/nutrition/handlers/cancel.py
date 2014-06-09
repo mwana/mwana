774bf4bcade3b919769d372dfc8cbfd01495e5e5
Lengani Kaunda <lengani@gmail.com>
2014-06-10 00:02:03 +0200

    Cancel GM reports by location, healthworker and child_id

--- mwana/apps/nutrition/handlers/cancel.py
+++ mwana/apps/nutrition/handlers/cancel.py
@@ -20,16 +20,40 @@ class CancelHandler(KeywordHandler):
     def help(self):
         self.respond(CANCEL_HELP)
 
+    def __identify_healthworker(self):
+        # return the healthworker if already registered on this connection
+        try:
+            if self.msg.connections[0].contact is not None:
+                healthworker = self.msg.connections[0].contact
+                return healthworker
+            else:
+                return None
+        except ObjectDoesNotExist:
+            return None
+
+    def __latest_assessment(self, patient, healthworker):
+        assessments = Assessment.objects.exclude(status='C').filter(
+            patient=patient,
+            healthworker=healthworker).order_by('-date')
+        if len(assessments) > 0:
+            return assessments[0]
+        else:
+            return None
+
     def handle(self, text):
         child = text.strip()
+        healthworker = self.__identify_healthworker()
+        if healthworker is None:
+            return self.respond(
+                "Please register as a Mobile Agent before using AnthroWatch.")
+        location = healthworker.clinic.slug
         try:
-            patient = Person.objects.get(code=child)
-            ass = patient.latest_assessment()
+            patient = Person.objects.get(code=child, cluster_id=location)
+            ass = self.__latest_assessment(patient, healthworker)
             if ass is not None:
                 ass.cancel()
                 self.respond(CANCEL_CONFIRM %
                              (ass.healthworker.name,
-                              ass.healthworker.interviewer_id,
                               ass.date, patient.code))
             else:
                 self.respond(CANCEL_ERROR % (child,))
