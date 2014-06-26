15fa5e74d769b6478346ef3213ec1828102cc350
tsinkala <sinkalation@gmail.com>
2014-06-26 22:32:58 +0200

    cleanup

--- mwana/apps/patienttracing/handlers/confirm.py
+++ mwana/apps/patienttracing/handlers/confirm.py
@@ -155,33 +155,4 @@ class ConfirmHandler(KeywordHandler):
         if initiator_contact.default_connection is not None:
             msg = OutgoingMessage(initiator_contact.default_connection, self.initiator_status_update_txt % (initiator_contact.name, self.msg.connection.contact.name, pat_name))
             msg.send()
-
-    
-
-# ====================================================================     
-#    DELETE ME!
-#
-#    initiator = models.ForeignKey(Contact, related_name='patients_traced',
-#                                     limit_choices_to={'types__slug': 'clinic_worker'},
-#                                     null=True, blank=True)
-#    type = models.CharField(max_length=15)
-#    name = models.CharField(max_length=50) # name of patient to trace
-#    patient_event = models.ForeignKey(PatientEvent, related_name='patient_traces',
-#                                      null=True, blank=True) 
-#    messenger = models.ForeignKey(Contact,  related_name='patients_reminded',
-#                            limit_choices_to={'types__slug': 'cba'}, null=True,
-#                            blank=True)# cba who informs patient
-#
-#    confirmed_by = models.ForeignKey(Contact, related_name='patient_traces_confirmed',
-#                            limit_choices_to={'types__slug': 'cba'}, null=True,
-#                            blank=True)# cba who confirms that patient visited clinic
-#
-#    status = models.CharField(choices=STATUS_CHOICES, max_length=15) # status of tracing activity
-#
-#    start_date = models.DateTimeField() # date when tracing starts
-#    reminded_on = models.DateTimeField(null=True, blank=True) # date when cba tells mother
-#    confirmed_date = models.DateTimeField(null=True, blank=True)# date of confirmation that patient visited clinic   
-#    
-# =======================================================================    
-    
-    
+ 
