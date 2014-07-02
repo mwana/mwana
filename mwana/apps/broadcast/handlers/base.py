d2bd77eeb080fe005128e9a043caff7272eb7f1e
Lengani Kaunda <lengani@gmail.com>
2014-07-02 09:38:07 +0200

    Update excluded groups in contacts.

--- mwana/apps/broadcast/handlers/base.py
+++ mwana/apps/broadcast/handlers/base.py
@@ -3,7 +3,7 @@ import logging
 from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
 from rapidsms.messages.outgoing import OutgoingMessage
 from mwana.apps.broadcast.models import BroadcastMessage
-from mwana.const import get_hub_worker_type
+from mwana.const import get_hub_worker_type, get_patient_type
 from rapidsms.router import send
 
 HELP_TEXT = "To send a message to %(group)s send keyword %(keyword)s followed by the contents of your message."
@@ -15,8 +15,9 @@ class BroadcastHandler(KeywordHandler):
     """Broadcast handler"""
 
     def help(self):
-        self.respond(HELP_TEXT, group=self.group_name,
-                     keyword=self.keyword.split('|')[0].upper())
+        self.respond(HELP_TEXT % dict(
+            group=self.group_name,
+            keyword=self.keyword.split('|')[0].upper()))
 
     @property
     def group_name(self):
@@ -26,15 +27,20 @@ class BroadcastHandler(KeywordHandler):
         vars = dict(user=self.msg.contact.name,
                     text=text, group=self.group_name)
         message_body = "%(text)s [from %(user)s to %(group)s]" % vars
-        for contact in contacts.exclude(types=get_hub_worker_type()):
+        excluded_groups = []
+        excluded_groups.append(get_hub_worker_type())
+        excluded_groups.append(get_patient_type())
+        for contact in contacts.exclude(types__in=excluded_groups):
             if contact.default_connection is None:
-                logger.info("Can't send to %s as they have no connections" % contact)
+                logger.info(
+                    "Can't send to %s as they have no connections" % contact)
             else:
                 send(message_body, [contact.default_connection])
 
         logger_msg = getattr(self.msg, "logger_msg", None)
         if not logger_msg:
-            logger.error("No logger message found for %s. Do you have the message log app running?" %\
+            logger.error(
+                "No logger message found for %s. Ensure the message log app running?" %\
                        self.msg)
         bmsg = BroadcastMessage.objects.create(logger_message=logger_msg,
                                                contact=self.msg.contact,
--- mwana/apps/broadcast/handlers/blaster.py
+++ mwana/apps/broadcast/handlers/blaster.py
@@ -5,12 +5,13 @@ from mwana.util import get_clinic_or_default
 
 
 class BlastHandler(BroadcastHandler):
-    
+
     group_name = "Mwana Users"
     keyword = "blast|blust|blaster|bluster|blastar|blustar|blasta|blusta"
-    
+
     def handle(self, text):
         if self.msg.contact is not None and self.msg.contact.is_help_admin and self.msg.contact.is_active:
             contacts = Contact.active.exclude(id=self.msg.contact.id)
             return self.broadcast(text, contacts)
-              
+        else:
+            self.respond(UNREGISTERED)
