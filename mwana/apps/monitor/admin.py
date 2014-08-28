ace80bb6f43ac79b52433fba4b54fc9c82f36c13
tsinkala <sinkalation@gmail.com>
2014-08-28 23:43:19 +0200

    Better admin, adding more fields for display and also for searching

--- mwana/apps/monitor/admin.py
+++ mwana/apps/monitor/admin.py
@@ -1,28 +1,37 @@
 # vim: ai ts=4 sts=4 et sw=4
-from mwana.apps.monitor.models import Support
+from django.contrib import admin
 from mwana.apps.monitor.models import LostContactsNotification
 from mwana.apps.monitor.models import MonitorMessageRecipient
-from django.contrib import admin
+from mwana.apps.monitor.models import Support
 
 class MonitorMessageRecipientAdmin(admin.ModelAdmin):
-    list_display = ("contact", "receive_sms",)
-    list_filter = ("contact", "receive_sms",)
+    list_display = ("contact", "receive_sms", )
+    list_filter = ("contact", "receive_sms", )
 admin.site.register(MonitorMessageRecipient, MonitorMessageRecipientAdmin)
 
 
 class LostContactsNotificationAdmin(admin.ModelAdmin):
-    list_display = ('sent_to', 'facility', 'date')
-    list_filter = ('date', 'sent_to', )
+    list_display = ('sent_to', 'province', 'district', 'facility', 'code', 'date')
+    list_filter = ('date', 'sent_to',)
+    search_fields = ('facility__name', 'facility__parent__name',
+                     'facility__parent__parent__name')
     date_hierarchy = 'date'
+
+    def province(self, obj):
+        return obj.facility.parent.parent
+
+    def district(self, obj):
+        return obj.facility.parent
+
+    def code(self, obj):
+        return obj.facility.slug
 
 admin.site.register(LostContactsNotification, LostContactsNotificationAdmin)
 
 
-from django.contrib import admin
-
 class SupportAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_active')
     list_filter = ('user', 'is_active')
-    list_editable = ('is_active',)
+    list_editable = ('is_active', )
 
 admin.site.register(Support, SupportAdmin)
