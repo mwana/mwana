c37fe456c92aabb14e98d79c0290e8914a8f250d
Lengani Kaunda <lengani@gmail.com>
2015-01-27 15:15:09 +0200

    Streamline flood reports.

--- mwana/apps/emergency/tables.py
+++ mwana/apps/emergency/tables.py
@@ -9,6 +9,15 @@ class FloodReportTable(TableReport):
     district = tables.Column(
         verbose_name='District',
         accessor='reported_by.contact.clinic.parent.name')
+    facility = tables.Column(
+        verbose_name='Facility',
+        accessor='reported_by.contact.clinic.name')
+    facility_id = tables.Column(
+        verbose_name='HMIS Code',
+        accessor='reported_by.contact.location')
+    reported_by = tables.Column(
+        verbose_name='Reported By',
+        accessor='reported_by.contact.name')
 
     def render_district(self, value, record):
         if record.reported_by.contact.clinic.parent.parent is not None:
@@ -18,7 +27,14 @@ class FloodReportTable(TableReport):
         else:
             return value
 
+    def render_facility_id(self, value, record):
+        if "zone" == record.reported_by.contact.location.type.slug:
+            return record.reported_by.contact.location.parent.slug
+        else:
+            return record.reported_by.contact.location.slug
+
     class Meta:
         model = FloodReport
         attrs = {'class': "table table-striped table-bordered table-condensed"}
-        exclude = ('id',)
+        exclude = ('id', 'status', 'comments', 'addressed_on', 'resolved_by',
+                   'notes',)
