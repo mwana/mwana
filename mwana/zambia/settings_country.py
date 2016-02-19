0599bb60c33cabb983f60244d8d3eb0d5f6b1d5b
tsinkala <sinkalation@gmail.com>
2016-02-19 15:48:48 +0200

    Changing default response

--- mwana/zambia/settings_country.py
+++ mwana/zambia/settings_country.py
@@ -34,6 +34,7 @@ INSTALLED_APPS.append("mwana.apps.surveillance")
 INSTALLED_APPS.append("mwana.apps.zm_languages")
 INSTALLED_APPS.append("mwana.apps.labtests")
 INSTALLED_APPS.append("mwana.apps.act")
+#INSTALLED_APPS.append("mwana.apps.vaccination")
 #INSTALLED_APPS.append("mwana.apps.reminders.experimental")
 INSTALLED_APPS.append("mwana.apps.errorhandling")
 INSTALLED_APPS.append("mwana.apps.export")
@@ -49,7 +50,7 @@ EMAIL_SUBJECT_PREFIX = '[Mwana] '
 EMAIL_HOST = 'localhost'
 DEFAULT_FROM_EMAIL = 'sinkalation@gmail.com'
 
-
+DEFAULT_RESPONSE = "It seems something went wrong. Respond with HELP to get assistance."
 
 TIME_ZONE = 'Africa/Lusaka'
 
