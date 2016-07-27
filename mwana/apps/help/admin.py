61b9795564fb5ba42e28dd93643f5e379dfccd32
tsinkala <sinkalation@gmail.com>
2016-07-27 12:20:27 +0200

    including contact's PIN in help admin page

--- mwana/apps/help/admin.py
+++ mwana/apps/help/admin.py
@@ -6,7 +6,7 @@ from mwana.apps.help import models as help
 
 
 class HelpRequestAdmin(admin.ModelAdmin):
-    list_display = ('parent_loc', 'location', 'requested_by', 'name', 'type',
+    list_display = ('parent_loc', 'location', 'requested_by', 'name', 'pin', 'type',
     'requested_on', 'additional_text','status',)
     list_filter = ('requested_on', 'status' )
     list_select_related = True
@@ -15,29 +15,36 @@ class HelpRequestAdmin(admin.ModelAdmin):
     'requested_by__contact__location__parent__name',
     'requested_by__contact__types__name',)
 
+    def pin(self, obj):
+        try:
+            return obj.requested_by.contact.pin
+        except AttributeError:
+            return ""
+
     def type(self, obj):
         try:
             return ",".join(type.name for type in obj.requested_by.contact.types.all())
-        except:
+        except AttributeError:
             return ""
 
     def parent_loc(self, obj):
         try:
             return obj.requested_by.contact.location.parent.name
-        except:
+        except AttributeError:
             return "Unknown"
 
     def location(self, obj):
         try:
             return obj.requested_by.contact.location.name
-        except:
+        except AttributeError:
             return "Unknown"
 
     def name(self, obj):
         try:
             return obj.requested_by.contact.name
-        except:
+        except AttributeError:
             return ""
+
 admin.site.register(help.HelpRequest, HelpRequestAdmin)
 
 
