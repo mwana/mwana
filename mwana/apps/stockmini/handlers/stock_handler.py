0688087be3841a40dc82eb6f284f01e1c3a3f36c
jmutale <jbmutale@gmail.com>
2016-12-02 12:35:38 +0200

    Refactored stock_handler.py to remove dependency on on import 'from dateutil.parser import parse'

--- mwana/apps/stockmini/handlers/stock_handler.py
+++ mwana/apps/stockmini/handlers/stock_handler.py
@@ -1,13 +1,11 @@
 # vim: ai ts=4 sts=4 et sw=4
 import datetime
 import re
-
+from datetime import date
 from mwana.apps.stockmini.models import StockOnHand
-
 from mwana.apps.stockmini.models import Stock
-from mwana.apps.stock.models import Transaction
 from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
-from dateutil.parser import parse
+
 
 _ = lambda s: s
 
@@ -28,14 +26,32 @@ class StockHandler(KeywordHandler):
     def help(self):
         self.respond(self.HELP_TEXT)
 
-    def _is_valid_date_string(self, date_text):
-        try:
-            if "/" not in date_text:
-                return False
-            parse(date_text)
-            return True
-        except ValueError:
-            return False
+    def _parse_date(self, date_str):
+        if date_str.strip().isdigit():
+            week_of_year = abs(int(date_str))
+            if week_of_year > 53:
+                return None
+                # don't go back too far
+                #            if week_of_year < int((date.today().strftime('%U'))) - 6:
+                #                return None
+
+            return week_of_year
+
+        text = date_str.strip()
+        text = re.sub(r"\s+", " ", text)
+        tokens = re.split("[\s|\-|/|\.]", date_str.strip())
+        tokens = [t for t in tokens if t]
+
+        if len(tokens) != 3:
+            return None
+
+        values = [val.strip() for val in tokens if val.strip().isdigit()]
+
+        if len(values) != 3:
+            return None
+
+        return date(int(values[2]), int(values[1]), int(values[0]))
+
 
     def handle(self, text):
         if not self.msg.contact:
@@ -52,20 +68,20 @@ class StockHandler(KeywordHandler):
         if my_text != " ":
             this_text = my_text.split()
             string_date = this_text[0]
-            if not self._is_valid_date_string(string_date):
-                self.respond("Please enter a valid date. The expected format is mm/dd/YYYY")
+            if not self._parse_date(string_date):
+                self.respond("Please enter a valid date. You entered %s" % string_date)
                 return True
-            date_part = string_date.split('/')
-            date = datetime.date(int(date_part[2]), int(date_part[1]), int(date_part[0]))
+            else:
+                my_date = self._parse_date(string_date)
             today = datetime.date.today()
             three_months_ago = today - datetime.timedelta(days=90)
-            if date > today:
+            if my_date > today:
                 self.respond("Sorry you can't enter a date after today's date. "
-                             "%s is a future date." % date.strftime('%d/%b/%Y'))
+                             "%s is a future date." % my_date.strftime('%d/%b/%Y'))
                 return True
-            elif date < three_months_ago:
+            elif my_date < three_months_ago:
                 self.respond("Sorry you can't enter a date more than three months ago. "
-                             "%s is too far in the past" % date.strftime('%d/%b/%Y'))
+                             "%s is too far in the past" % my_date.strftime('%d/%b/%Y'))
                 return True
             s = Stock.objects.get(code=this_text[1].strip())
             level = this_text[2]
@@ -78,12 +94,12 @@ class StockHandler(KeywordHandler):
 
             location = self.msg.contact.location
 
-            if StockOnHand.objects.filter(date=date, stock=s, facility=location):
-                stock_on_hand = StockOnHand.objects.get(date=date, stock=s, facility=location)
+            if StockOnHand.objects.filter(date=my_date, stock=s, facility=location):
+                stock_on_hand = StockOnHand.objects.get(date=my_date, stock=s, facility=location)
                 stock_on_hand.level = level
                 stock_on_hand.save()
             else:
-                stock_on_hand = StockOnHand.objects.create(date=date, stock=s, level=level, facility=location)
+                stock_on_hand = StockOnHand.objects.create(date=my_date, stock=s, level=level, facility=location)
 
             self.respond(
                 "Thank you for reporting the stock level as {0:d} for {1:s} on {2:s}. "
