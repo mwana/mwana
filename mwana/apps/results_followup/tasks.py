27138bb7c42d8b98307dfce0b5909770875d49c2
tsinkala <sinkalation@gmail.com>
2017-07-11 10:38:16 +0200

    automatically send email alerts for vl results

--- mwana/apps/results_followup/tasks.py
+++ mwana/apps/results_followup/tasks.py
@@ -1,6 +1,7 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 import logging
+from mwana.apps.results_followup.management.commands.create_followup_results import update_viral_load_alert
 from mwana.apps.results_followup.management.commands.create_followup_results import update_infant_result_alert
 
 _ = lambda s: s
@@ -12,3 +13,4 @@ logger = logging.getLogger('mwana.apps.results_followup.tasks')
 def send_email_alerts(router):
     logger.info('Sending email messages')
     update_infant_result_alert()
+    update_viral_load_alert()
