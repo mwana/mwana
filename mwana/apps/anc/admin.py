099f0eaf2bd5176bea3eb873d38e18cea4eaafa3
tsinkala <sinkalation@gmail.com>
2017-10-09 09:55:30 +0200

    Fixed import error

--- mwana/apps/anc/admin.py
+++ mwana/apps/anc/admin.py
@@ -1,6 +1,7 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 from django.contrib import admin
+from mwana.apps.anc.models import CommunityWorker
 from mwana.apps.anc.models import SentCHWMessage
 
 from mwana.apps.anc.models import EducationalMessage, SentClientMessage, Client
